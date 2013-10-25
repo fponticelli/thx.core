11b373a7d1b4be63e02b31c0a4e8ccbb4c62e7c2
Franco Ponticelli <franco.ponticelli@gmail.com>
2013-10-25 12:33:11 -0600

    min int added

--- src/thx/core/Ints.hx
+++ src/thx/core/Ints.hx
@@ -11,11 +11,21 @@ class Ints
 	{
 		return v < min ? min : (v > max ? max : v);
 	}
-	
+
 	static var pattern_parse = ~/^[+-]?(\d+|0x[0-9A-F]+)$/i;
 	public static function canParse(s : String)
 	{
 		return pattern_parse.match(s);
+	}
+
+	public inline static function min(a : Int, b : Int)
+	{
+		return a < b ? a : b;
+	}
+
+	public inline static function max(a : Int, b : Int)
+	{
+		return a > b ? a : b;
 	}
 
 	// TODO add proper octal/hex/exp support
