5817078f3da15fcab8d3442959487b014bfc0407
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-08-28 13:19:34 -0600

    added files to release

--- submit.sh
+++ submit.sh
@@ -1,3 +1,3 @@
 rm thx.core.zip
-zip -r thx.core.zip hxml src test extraParams.hxml haxelib.json
+zip -r thx.core.zip hxml src test extraParams.hxml haxelib.json LICENSE README.md
 haxelib submit thx.core.zip
