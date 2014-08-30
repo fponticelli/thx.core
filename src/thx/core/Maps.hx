dd1d16c8695a3911d482c2620daacc6e146919d3
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-08-30 00:31:52 -0600

    changed signature of thx.core.Maps.tuples and removed orphan file

--- src/thx/core/Maps.hx
+++ src/thx/core/Maps.hx
@@ -1,5 +1,6 @@
 package thx.core;
 
+import Map;
 import thx.core.Tuple;
 using thx.core.Iterators;
 
@@ -10,7 +11,7 @@ class Maps {
     /**
      * Converts a Map<TKey, TValue> into an Array<Tuple2<TKey, TValue>>
      */
-    public static function tuples<TKey, TValue>(map: Map<TKey, TValue>): Array<Tuple2<TKey, TValue>>
+    public static function tuples<TKey, TValue>(map: IMap<TKey, TValue>): Array<Tuple2<TKey, TValue>>
         return map.keys().map(function(key)
             return new Tuple2(key, map.get(key))
         );
