0d31ebe7c303b669239feb103db4e685ce42654c
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-22 07:20:34 -0600

    renamed toTupleX to with() and aded drop methods

--- src/thx/core/Tuple.hx
+++ src/thx/core/Tuple.hx
@@ -4,7 +4,7 @@ abstract Tuple0(Nil) {
   inline public function new()
     this = nil;
 
-  inline public function toTuple1<T0>(v : T0)
+  inline public function with<T0>(v : T0)
     return new Tuple1(v);
 
   inline public function toString()
@@ -23,8 +23,14 @@ abstract Tuple1<T0>(T0) {
   public var _0(get, never) : T0;
   inline function get__0() return this;
 
-  inline public function toTuple2<T1>(v : T1)
+  inline public function with<T1>(v : T1)
     return new Tuple2(_0, v);
+
+  inline public function dropLeft()
+    return new Tuple0();
+
+  inline public function dropRight()
+    return new Tuple0();
 
   inline public function toString()
     return 'Tuple1($_0)';
@@ -41,7 +47,13 @@ abstract Tuple2<T0, T1>(TTuple2<T0, T1>) from TTuple2<T0, T1> to TTuple2<T0, T1>
   inline public function flip() : Tuple2<T1, T0>
     return { _0 : this._1, _1 : this._0 };
 
-  inline public function toTuple3<T2>(v : T2)
+  inline public function dropLeft()
+    return new Tuple1(this._1);
+
+  inline public function dropRight()
+    return new Tuple1(this._0);
+
+  inline public function with<T2>(v : T2)
     return new Tuple3(_0, _1, v);
 
   inline public function toString()
@@ -63,7 +75,13 @@ abstract Tuple3<T0, T1, T2>(TTuple3<T0, T1, T2>) from TTuple3<T0, T1, T2> to TTu
   inline public function flip() : Tuple3<T2, T1, T0>
     return { _0 : this._2, _1 : this._1, _2 : this._0 };
 
-  inline public function toTuple4<T3>(v : T3)
+  inline public function dropLeft()
+    return new Tuple2(this._1, this._2);
+
+  inline public function dropRight()
+    return new Tuple2(this._0, this._1);
+
+  inline public function with<T3>(v : T3)
     return new Tuple4(_0, _1, _2, v);
 
   inline public function toString()
@@ -87,7 +105,13 @@ abstract Tuple4<T0, T1, T2, T3>(TTuple4<T0, T1, T2, T3>) from TTuple4<T0, T1, T2
   inline public function flip() : Tuple4<T3, T2, T1, T0>
     return { _0 : this._3, _1 : this._2, _2 : this._1, _3 : this._0 };
 
-  inline public function toTuple5<T4>(v : T4)
+  inline public function dropLeft()
+    return new Tuple3(this._1, this._2, this._3);
+
+  inline public function dropRight()
+    return new Tuple3(this._0, this._1, this._2);
+
+  inline public function with<T4>(v : T4)
     return new Tuple5(_0, _1, _2, _3, v);
 
   inline public function toString()
@@ -113,7 +137,13 @@ abstract Tuple5<T0, T1, T2, T3, T4>(TTuple5<T0, T1, T2, T3, T4>) from TTuple5<T0
   inline public function flip() : Tuple5<T4, T3, T2, T1, T0>
     return { _0 : this._4, _1 : this._3, _2 : this._2, _3 : this._1, _4 : this._0 };
 
-  inline public function toTuple6<T5>(v : T5)
+  inline public function dropLeft()
+    return new Tuple4(this._1, this._2, this._3, this._4);
+
+  inline public function dropRight()
+    return new Tuple4(this._0, this._1, this._2, this._3);
+
+  inline public function with<T5>(v : T5)
     return new Tuple6(_0, _1, _2, _3, _4, v);
 
   inline public function toString()
@@ -140,6 +170,12 @@ abstract Tuple6<T0, T1, T2, T3, T4, T5>(TTuple6<T0, T1, T2, T3, T4, T5>) from TT
 
   inline public function flip() : Tuple6<T5, T4, T3, T2, T1, T0>
     return { _0 : this._5, _1 : this._4, _2 : this._3, _3 : this._2, _4 : this._1, _5 : this._0 };
+
+  inline public function dropLeft()
+    return new Tuple5(this._1, this._2, this._3, this._4, this._5);
+
+  inline public function dropRight()
+    return new Tuple5(this._0, this._1, this._2, this._3, this._4);
 
   inline public function toString()
     return 'Tuple6($_0,$_1,$_2,$_3,$_4,$_5)';
