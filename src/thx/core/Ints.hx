a514568507a63731009e4a515f999260666c099a
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-24 08:05:24 -0600

    reordered methods

--- src/thx/core/Ints.hx
+++ src/thx/core/Ints.hx
@@ -6,18 +6,21 @@
 package thx.core;
 
 class Ints {
-  public static inline function clamp(v : Int, min : Int, max : Int) : Int
-    return v < min ? min : (v > max ? max : v);
-
   static var pattern_parse = ~/^[+-]?(\d+|0x[0-9A-F]+)$/i;
   public static function canParse(s : String)
     return pattern_parse.match(s);
 
-  public inline static function min(a : Int, b : Int)
-    return a < b ? a : b;
+  public static inline function clamp(v : Int, min : Int, max : Int) : Int
+    return v < min ? min : (v > max ? max : v);
+
+  public inline static function compare(a : Int, b : Int)
+    return a - b;
 
   public inline static function max(a : Int, b : Int)
     return a > b ? a : b;
+
+  public inline static function min(a : Int, b : Int)
+    return a < b ? a : b;
 
   // TODO add proper octal/hex/exp support
   public static function parse(s : String) {
@@ -25,9 +28,6 @@ class Ints {
       s = s.substr(1);
     return Std.parseInt(s);
   }
-
-  public inline static function compare(a : Int, b : Int)
-    return a - b;
 
   public static function range(start : Int, ?stop : Int, step = 1) : Array<Int> {
     if (null == stop) {
