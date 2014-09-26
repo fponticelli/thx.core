240f05d1fd248b66988d60d0a1623ca442c1cc36
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-25 22:57:34 -0600

    added Defaults.opt()

--- src/thx/core/Defaults.hx
+++ src/thx/core/Defaults.hx
@@ -3,6 +3,7 @@ package thx.core;
 #if macro
 import haxe.macro.Expr;
 import haxe.macro.ExprTools;
+import haxe.macro.Context;
 #end
 
 class Defaults {
@@ -29,9 +30,62 @@ Notice that the subject `value` must be a constant identifier (eg: fields, local
         throw '"or" method can only be used on constant identifiers (eg: fields, local variables, ...)';
     }
   }
+
+/**
+It traverses a chain of dot/array identifiers and it returns the last value in the chain or null if any of the identifiers is not set.
+
+```haxe
+var o : { a : { b : { c : String }}} = null;
+trace((o.a.b.c).opt()); // prints null
+var o = { a : { b : { c : 'A' }}};
+trace((o.a.b.c).opt()); // prints 'A'
+```
+**/
+  macro public static function opt(expr : Expr) {
+    var ids = [];
+    function traverse(e : haxe.macro.Type.TypedExprDef) switch e {
+      case TArray(a, e):
+        traverse(a.expr);
+        traverse(e.expr);
+      case TConst(TThis):
+        ids.push('this');
+      case TConst(TInt(index)):
+        ids.push('$index');
+      case TLocal(o):
+        ids.push(o.name);
+      case TField(f, v):
+        traverse(f.expr);
+        switch v {
+          case FAnon(id):
+            ids.push(id.toString());
+          case FInstance(_, n):
+            ids.push(n.toString());
+          case _:
+            throw 'invalid expression $e';
+        }
+      case TParenthesis(p):
+        traverse(p.expr);
+      case _:
+        throw 'invalid expression $e';
+    }
+    traverse(Context.typeExpr(expr).expr);
+    var first = ids.shift(),
+        buf   = '(function(_) { if(null == _) return null; ',
+        path  = '_';
+    for(id in ids) {
+      if(thx.core.Ints.canParse(id)) {
+        path = '$path[$id]';
+      } else {
+        path = path == '' ? id : '$path.$id';
+      }
+      buf += 'if(null == $path) return null; ';
+    }
+    buf += 'return $path; })($first)';
+    return Context.parse(buf , expr.pos);
+  }
 }
 
 /* TODO:
-11:49:51  Simn:  You could Context.typeExpr it I guess. 
-11:50:00  Simn:  And check if that returns something harmless. 
+11:49:51  Simn:  You could Context.typeExpr it I guess.
+11:50:00  Simn:  And check if that returns something harmless.
 */
--- test/thx/core/TestDefaults.hx
+++ test/thx/core/TestDefaults.hx
@@ -7,13 +7,31 @@ package thx.core;
 
 import utest.Assert;
 using thx.core.Defaults;
+import thx.core.Defaults.*;
 
 class TestDefaults {
   public var withValue = "A";
   public var withoutValue : String;
+  public var getter(get, null) : String;
+  public var setter(get, set) : String;
+  function get_getter() return 'A';
+
+  var v : String;
+  function get_setter() return v;
+  function set_setter(v : String) return this.v = v;
+
+  public var nested : { a : { b : String }};
+  public var empty : { a : { b : String }};
+
   public function new() { }
 
-  public function testFeatures() {
+  public function setup() {
+    this.v = null;
+    this.nested = { a : { b : 'A' } };
+    this.empty = null;
+  }
+
+  public function testOr() {
     var s : String = null;
     Assert.equals('B', s.or('B'));
     s = 'A';
@@ -37,5 +55,59 @@ class TestDefaults {
 
     Assert.equals('B', Defaults.or(o.b, 'B'));
     Assert.equals('B', o.b.or('B'));
+
+    Assert.equals('A', Defaults.or(getter, 'B'));
+    Assert.equals('A', getter.or('B'));
+
+    Assert.equals('B', Defaults.or(setter, 'B'));
+    Assert.equals('B', setter.or('B'));
+
+    setter = 'A';
+    Assert.equals('A', Defaults.or(setter, 'B'));
+    Assert.equals('A', setter.or('B'));
+  }
+
+  public function testOpt() {
+    var o : { a : { b : { c : String }}} = null;
+    Assert.isNull((o.a.b.c).opt());
+    Assert.isNull(Defaults.opt(o.a.b.c));
+    Assert.isNull(Defaults.opt(o.a.b.c));
+    Assert.isNull((o.a.b.c).opt());
+    Assert.equals('B', (o.a.b.c).opt().or('B'));
+
+    o = { a : { b : { c : 'A' } } };
+    Assert.equals('A', (o.a.b.c).opt());
+    Assert.equals('A', (o.a.b.c).opt().or('B'));
+
+    Assert.same({ c : 'A'}, (o.a.b).opt());
+    Assert.same({ c : 'A'}, (o.a.b).opt().or({ c : 'B'}));
+
+    o = { a : { b : null } };
+    Assert.isNull((o.a.b.c).opt());
+    Assert.equals('B', (o.a.b.c).opt().or('B'));
+
+    Assert.isNull((o.a.b).opt());
+    Assert.same({ c : 'B'}, (o.a.b).opt().or({ c : 'B'}));
+
+    Assert.equals('A', (this.nested.a.b).opt());
+    Assert.equals('A', (nested.a.b).opt());
+
+    Assert.isNull((this.empty.a.b).opt());
+    Assert.isNull((empty.a.b).opt());
+
+    var arr : { a : Array<{ b : String }> } = null;
+    Assert.isNull((arr.a[0].b).opt());
+    arr = { a : [{ b : 'A' }] };
+    Assert.isNull((arr.a[1].b).opt());
+    Assert.equals('A', (arr.a[0].b).opt());
+
+    var arr : Array<Array<Array<Int>>> = null;
+    Assert.isNull((arr[0]).opt());
+    Assert.isNull((arr[0][1]).opt());
+    Assert.isNull((arr[0][1][3]).opt());
+
+    arr = [[[1,2,3],[4,5,6]]];
+    Assert.equals(1, (arr[0][0][0]).opt());
+    Assert.equals(6, (arr[0][1][2]).opt());
   }
 }
