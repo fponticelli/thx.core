a5f85ff0d45dfca883c1b0d09e89ada36f11031a
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-26 21:29:51 -0600

    documented Floats and some of Functions

--- src/thx/core/Floats.hx
+++ src/thx/core/Floats.hx
@@ -5,6 +5,9 @@
 
 package thx.core;
 
+/**
+`Floats` contains helper methods to work with `Float` values.
+**/
 class Floats {
   public static inline var TOLERANCE : Float = 10e-5;
   public static inline var EPSILON : Float = 10e-10;
@@ -28,36 +31,70 @@ trace(-0.5.clamp(0, 1)); // prints 0.0
   public static inline function clamp(v : Float, min : Float, max : Float) : Float
     return v < min ? min : (v > max ? max : v);
 
+/**
+Like clamp but you only pass one argument (`max`) that is used as the upper limit
+and the opposite (additive inverse or `-max`) as the lower limit.
+**/
   public static inline function clampSym(v : Float, max : Float) : Float
     return clamp(v, -max, max);
 
+/**
+It returns the comparison value (an integer number) between two `float` values.
+**/
   inline public static function compare(a : Float, b : Float) : Int
     return a < b ? -1 : (b > a ? 1 : 0);
 
-  public static function interpolateBetween(t : Float, a : Float, b : Float)
+/**
+`interpolate` returns a value between `a` and `b` for any value of `t` between 0 and 1.
+**/
+  public static function interpolate(t : Float, a : Float, b : Float)
     return (b - a) * t + a;
 
-  inline public static function nearEqual(a : Float, b : Float)
+/**
+Float numbers can sometime introduce tiny errors even for simple operations. `nearEquals`
+comapres floats using a tiny tollerance defined as `EPSILON`.
+**/
+  inline public static function nearEquals(a : Float, b : Float)
     return Math.abs(a - b) <= EPSILON;
 
+/**
+`nearZero` finds if the passed number is zero or very close to it. `EPSILON` is used
+as the tollerance value.
+**/
   inline public static function nearZero(n : Float)
     return Math.abs(n) <= EPSILON;
 
+
+/**
+`normalize` clamps the passwed value between 0 and 1.
+**/
   public static inline function normalize(v : Float) : Float
     return clamp(v, 0, 1);
 
+/**
+`parse` can parse a string and tranform it into a `Float` value.
+**/
   public static function parse(s : String) {
     if (s.substr(0, 1) == "+")
       s = s.substr(1);
     return Std.parseFloat(s);
   }
 
+/**
+Passed two boundaries values (`min`, `max`), `wrap` ensure that the passed vale `v` will
+be included in the boundaries. If the value exceeds `max`, the value is reduced by `min`
+repeatedely until it falls within the range. Similar treatment is done if value is
+below `min`.
+**/
   public static function wrap(v : Float, min : Float, max : Float) : Float {
     var range = max - min + 1;
     if (v < min) v += range * ((min - v) / range + 1);
     return min + (v - min) % range;
   }
 
+/**
+Similar to `wrap`, it works for numbers between 0 and `max`.
+**/
   public static function wrapCircular(v : Float, max : Float) : Float {
     v = v % max;
     if (v < 0)
--- src/thx/core/Functions.hx
+++ src/thx/core/Functions.hx
@@ -1,5 +1,8 @@
 package thx.core;
 
+/**
+Helper functions for functions with arity 0 (functions that do not take arguments).
+**/
 class Function0 {
   public inline static function join(fa : Void -> Void, fb : Void -> Void)
     return function() {
@@ -16,6 +19,9 @@ class Function0 {
     };
 }
 
+/**
+Helper functions for functions with arity 1 (functions that take exactly 1 argument).
+**/
 class Function1 {
   public inline static function compose<TIn, TRet1, TRet2>(fa : TRet2 -> TRet1, fb : TIn -> TRet2)
     return function(v : TIn) return fa(fb(v));
@@ -29,6 +35,9 @@ class Function1 {
     }
 }
 
+/**
+Generic helper for functions.
+**/
 class Functions {
   public static function equality<T>(a : T, b : T) return a == b;
 }
