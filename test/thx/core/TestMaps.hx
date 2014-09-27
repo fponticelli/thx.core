c17b5c0459b389a79f8e2c6cfdfac2034e1c9732
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-27 13:55:23 -0600

    minor

--- test/thx/core/TestMaps.hx
+++ test/thx/core/TestMaps.hx
@@ -2,23 +2,21 @@ package thx.core;
 
 import utest.Assert;
 
-class TestMaps
-{
-    public function new() { }
+class TestMaps {
+  public function new() { }
 
-    public function testTuples()
-    {
-        var map = [
-            "key1" => 1,
-            "key2" => 2
-        ];
+  public function testTuples() {
+    var map = [
+      "key1" => 1,
+      "key2" => 2
+    ];
 
-        var tuples = Maps.tuples(map);
+    var tuples = Maps.tuples(map);
 
-        Assert.equals(2, tuples.length);
-        Assert.equals(tuples[0]._0, "key1");
-        Assert.equals(tuples[0]._1, 1);
-        Assert.equals(tuples[1]._0, "key2");
-        Assert.equals(tuples[1]._1, 2);
-    }
+    Assert.equals(2, tuples.length);
+    Assert.equals(tuples[0]._0, "key1");
+    Assert.equals(tuples[0]._1, 1);
+    Assert.equals(tuples[1]._0, "key2");
+    Assert.equals(tuples[1]._1, 2);
+  }
 }
