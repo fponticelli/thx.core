70beb4ef70a86d3b97caa53a9f7a736302cc711b
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-27 15:12:56 -0600

    document Iterators

--- src/thx/core/Iterators.hx
+++ src/thx/core/Iterators.hx
@@ -10,9 +10,15 @@ Helper class to work with `Iterator`.
 For documentation of specific methods refer to the equivalent methods in `thx.core.Arrays`;
 **/
 class Iterators {
+/**
+Refer to `thx.core.Arrays.eachPair`.
+**/
   public static function eachPair<TIn, TOut>(it : Iterator<TIn>, handler : TIn -> TIn -> Bool)
     Arrays.eachPair(toArray(it), handler);
 
+/**
+Refer to `Array.filter`.
+**/
   public static function filter<TItem>(it : Iterator<TItem>, predicate : TItem -> Bool) : Array<TItem>
     return reduce(it, function(acc : Array<TItem>, item) {
         if(predicate(item))
@@ -20,9 +26,15 @@ class Iterators {
         return acc;
       }, []);
 
+/**
+Refer to `thx.core.Arrays.filterPluck`.
+**/
   macro public static function filterPluck<T>(it : ExprOf<Iterator<T>>, expr : ExprOf<Bool>) : ExprOf<Array<T>>
     return macro thx.core.Iterators.filter($e{it}, function(_) return $e{expr});
 
+/**
+Refer to `thx.core.Arrays.find`.
+**/
   public static function find<T, TFind>(it : Iterator<T>, f : T -> Bool) : Null<T> {
     for(item in it)
       if(f(item))
@@ -30,9 +42,15 @@ class Iterators {
     return null;
   }
 
+/**
+Refer to `thx.core.Arrays.first`.
+**/
   public static function first<T, TFind>(it : Iterator<T>) : Null<T>
     return it.hasNext() ? it.next() : null;
 
+/**
+Refer to `thx.core.Arrays.isEmpty`.
+**/
   inline public static function isEmpty<T>(it : Iterator<T>) : Bool
     return !it.hasNext();
 
@@ -48,12 +66,18 @@ has two fields `next` and `hasNext` and that they are both functions.
     return Reflect.isFunction(Reflect.field(v, "next")) && Reflect.isFunction(Reflect.field(v, "hasNext"));
   }
 
+/**
+Refer to `thx.core.Arrays.last`.
+**/
   public static function last<T, TFind>(it : Iterator<T>) : Null<T> {
     var buf = null;
     while(it.hasNext()) buf = it.next();
     return buf;
   }
 
+/**
+Refer to `Array.map`.
+**/
   public static function map<T, S>(it : Iterator<T>, f : T -> S) : Array<S> {
     var acc = [];
     for(v in it)
@@ -61,6 +85,9 @@ has two fields `next` and `hasNext` and that they are both functions.
     return acc;
   }
 
+/**
+Refer to `thx.core.Arrays.mapi`.
+**/
   public static function mapi<T, S>(it : Iterator<T>, f : T -> Int -> S) : Array<S> {
     var acc = [],
         i = 0;
@@ -69,23 +96,38 @@ has two fields `next` and `hasNext` and that they are both functions.
     return acc;
   }
 
+/**
+Refer to `thx.core.Arrays.order`.
+**/
   public static function order<T>(it : Iterator<T>, sort : T -> T -> Int) {
     var n = Iterators.toArray(it);
     n.sort(sort);
     return n;
   }
 
+/**
+Refer to `thx.core.Arrays.pluck`.
+**/
   macro public static function pluck<T, TOut>(it : ExprOf<Iterator<T>>, expr : ExprOf<TOut>) : ExprOf<Array<TOut>>
     return macro thx.core.Iterators.map($e{it}, function(_) return ${expr});
 
+/**
+Refer to `thx.core.Arrays.plucki`.
+**/
   macro public static function plucki<T, TOut>(it : ExprOf<Iterator<T>>, expr : ExprOf<TOut>) : ExprOf<Array<TOut>>
     return macro thx.core.Iterators.mapi($e{it}, function(_, i) return ${expr});
 
+/**
+Refer to `thx.core.Arrays.reduce`.
+**/
   public static function reduce<TItem, TAcc>(it : Iterator<TItem>, callback : TAcc -> TItem -> TAcc, initial : TAcc) : TAcc {
     map(it, function(v) initial = callback(initial, v));
     return initial;
   }
 
+/**
+Refer to `thx.core.Arrays.reducei`.
+**/
   public static function reducei<TItem, TAcc>(it : Iterator<TItem>, callback : TAcc -> TItem -> Int -> TAcc, initial : TAcc) : TAcc {
     mapi(it, function(v, i) initial = callback(initial, v, i));
     return initial;
