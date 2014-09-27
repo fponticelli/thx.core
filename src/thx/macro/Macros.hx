8f08855450281bd6cd4e8c17020a3605b470f782
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-27 14:09:09 -0600

    documented Macros.

--- src/thx/macro/Macros.hx
+++ src/thx/macro/Macros.hx
@@ -4,13 +4,28 @@ package thx.macro;
 import haxe.macro.Context;
 import haxe.macro.Expr;
 
+/**
+Helper methods to use inside macro context.
+**/
 class Macros {
+/**
+Given a fully qualified path to a type, it returns the path to the file module
+containing that type.
+
+If the type is not found, an exception is thrown.
+**/
   public static function getModulePath(type : String) : String
     return switch Context.getType(type) {
       case TInst(t, _): Context.getPosInfos(t.get().pos).file;
       case _: throw 'invalid type $type';
     };
 
+/**
+Given a relative file path, it returns the absolute path to that file if it is encountered
+within any of the available class path.
+
+It returns null if the file is not found in the class paths.
+**/
   public static function getFileInClassPath(file : String) : String {
     for(path in Context.getClassPath()) {
       var fullpath = path+file;
@@ -19,6 +34,5 @@ class Macros {
     }
     return null;
   }
-
 }
 #end
