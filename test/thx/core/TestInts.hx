d4a95430830a7bb63cbb60481a68b44c22673dfa
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-27 10:43:28 -0600

    added tests for Ints.range()

--- test/thx/core/TestInts.hx
+++ test/thx/core/TestInts.hx
@@ -7,7 +7,18 @@ package thx.core;
 
 import utest.Assert;
 
-class TestInts
-{
+class TestInts {
 	public function new() { }
+
+  public function testRange() {
+    var range = Ints.range(2, 7, 1);
+    Assert.same([2,3,4,5,6], range);
+    range = Ints.range(2, 7, 2);
+    Assert.same([2,4,6], range);
+    range = Ints.range(2, 7, 3);
+    Assert.same([2,5], range);
+
+    range = Ints.range(7, 2, -2);
+    Assert.same([7,5,3], range);
+  }
 }
