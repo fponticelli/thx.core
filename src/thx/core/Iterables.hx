5c67408ae7eb2a6db6c9f632d7c779ced1219863
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-27 10:43:53 -0600

    updated Iterators and Iterables

--- src/thx/core/Iterables.hx
+++ src/thx/core/Iterables.hx
@@ -1,5 +1,9 @@
 package thx.core;
 
+#if macro
+import haxe.macro.Expr;
+#end
+
 class Iterables {
   public inline static function eachPair<TIn, TOut>(it : Iterable<TIn>, handler : TIn -> TIn -> Bool)
     return Iterators.eachPair(it.iterator(), handler);
@@ -7,11 +11,17 @@ class Iterables {
   public inline static function filter<T>(it : Iterable<T>, predicate : T -> Bool) : Array<T>
     return Iterators.filter(it.iterator(), predicate);
 
-  public inline static function find<T, TFind>(it : Iterable<T>, f : T -> Bool)
-    return Iterators.find(it.iterator(), f);
+  macro public static function filterPluck<T>(it : ExprOf<Iterable<T>>, expr : ExprOf<Bool>) : ExprOf<Array<T>>
+    return macro thx.core.Iterators.filter($e{it}.iterator(), function(_) return $e{expr});
 
-  public inline static function first<T, TFind>(it : Iterable<T>, f : T -> Bool)
-    return Iterators.first(it.iterator(), f);
+  public inline static function find<T, TFind>(it : Iterable<T>, predicate : T -> Bool) : Null<T>
+    return Iterators.find(it.iterator(), predicate);
+
+  public inline static function first<T, TFind>(it : Iterable<T>) : Null<T>
+    return Iterators.first(it.iterator());
+
+  public inline static function last<T, TFind>(it : Iterable<T>) : Null<T>
+    return Iterators.last(it.iterator());
 
   public inline static function isEmpty<T>(it : Iterable<T>) : Bool
     return Iterators.isEmpty(it.iterator());
@@ -30,6 +40,12 @@ class Iterables {
 
   public inline static function order<T>(it : Iterable<T>, sort : T -> T -> Int) : Array<T>
     return Iterators.order(it.iterator(), sort);
+
+  macro public static function pluck<T, TOut>(it : ExprOf<Iterable<T>>, expr : ExprOf<TOut>) : ExprOf<Array<TOut>>
+    return macro thx.core.Iterators.map($e{it}.iterator(), function(_) return ${expr});
+
+  macro public static function plucki<T, TOut>(it : ExprOf<Iterable<T>>, expr : ExprOf<TOut>) : ExprOf<Array<TOut>>
+    return macro thx.core.Iterators.mapi($e{it}.iterator(), function(_, i) return ${expr});
 
   public inline static function reduce<TItem, TAcc>(it : Iterable<TItem>, callback : TAcc -> TItem -> TAcc, initial : TAcc) : TAcc
     return Iterators.reduce(it.iterator(), callback, initial);
--- src/thx/core/Iterators.hx
+++ src/thx/core/Iterators.hx
@@ -1,6 +1,48 @@
 package thx.core;
 
+#if macro
+import haxe.macro.Expr;
+#end
+
 class Iterators {
+  public static function eachPair<TIn, TOut>(it : Iterator<TIn>, handler : TIn -> TIn -> Bool)
+    Arrays.eachPair(toArray(it), handler);
+
+  public static function filter<TItem>(it : Iterator<TItem>, predicate : TItem -> Bool) : Array<TItem>
+    return reduce(it, function(acc : Array<TItem>, item) {
+        if(predicate(item))
+          acc.push(item);
+        return acc;
+      }, []);
+
+  macro public static function filterPluck<T>(it : ExprOf<Iterator<T>>, expr : ExprOf<Bool>) : ExprOf<Array<T>>
+    return macro thx.core.Iterators.filter($e{it}, function(_) return $e{expr});
+
+  public static function find<T, TFind>(it : Iterator<T>, f : T -> Bool) : Null<T> {
+    for(item in it)
+      if(f(item))
+        return item;
+    return null;
+  }
+
+  public static function first<T, TFind>(it : Iterator<T>) : Null<T>
+    return it.hasNext() ? it.next() : null;
+
+  inline public static function isEmpty<T>(it : Iterator<T>) : Bool
+    return !it.hasNext();
+
+  public static function isIterator(v : Dynamic) {
+    var fields = Types.isAnonymousObject(v) ? Reflect.fields(v) : Type.getInstanceFields(Type.getClass(v));
+    if(!Lambda.has(fields, "next") || !Lambda.has(fields, "hasNext")) return false;
+    return Reflect.isFunction(Reflect.field(v, "next")) && Reflect.isFunction(Reflect.field(v, "hasNext"));
+  }
+
+  public static function last<T, TFind>(it : Iterator<T>) : Null<T> {
+    var buf = null;
+    while(it.hasNext()) buf = it.next();
+    return buf;
+  }
+
   public static function map<T, S>(it : Iterator<T>, f : T -> S) : Array<S> {
     var acc = [];
     for(v in it)
@@ -10,35 +52,10 @@ class Iterators {
 
   public static function mapi<T, S>(it : Iterator<T>, f : T -> Int -> S) : Array<S> {
     var acc = [],
-      i = 0;
+        i = 0;
     for(v in it)
       acc.push(f(v, i++));
     return acc;
-  }
-
-  public static function first<T, TFind>(it : Iterator<T>, f : T -> Bool) {
-    for(item in it)
-      if(f(item))
-        return item;
-    return null;
-  }
-
-  public static function find<T, TFind>(it : Iterator<T>, f : T -> Bool) {
-    var out = [];
-    for(item in it)
-      if(f(item))
-        out.push(item);
-    return out;
-  }
-
-  public static function eachPair<TIn, TOut>(it : Iterator<TIn>, handler : TIn -> TIn -> Bool)
-    Arrays.eachPair(toArray(it), handler);
-
-  public static function toArray<T>(it : Iterator<T>) : Array<T> {
-    var items = [];
-    for(item in it)
-      items.push(item);
-    return items;
   }
 
   public static function order<T>(it : Iterator<T>, sort : T -> T -> Int) {
@@ -46,6 +63,12 @@ class Iterators {
     n.sort(sort);
     return n;
   }
+
+  macro public static function pluck<T, TOut>(it : ExprOf<Iterator<T>>, expr : ExprOf<TOut>) : ExprOf<Array<TOut>>
+    return macro thx.core.Iterators.map($e{it}, function(_) return ${expr});
+
+  macro public static function plucki<T, TOut>(it : ExprOf<Iterator<T>>, expr : ExprOf<TOut>) : ExprOf<Array<TOut>>
+    return macro thx.core.Iterators.mapi($e{it}, function(_, i) return ${expr});
 
   public static function reduce<TItem, TAcc>(it : Iterator<TItem>, callback : TAcc -> TItem -> TAcc, initial : TAcc) : TAcc {
     map(it, function(v) initial = callback(initial, v));
@@ -57,19 +80,11 @@ class Iterators {
     return initial;
   }
 
-  inline public static function isEmpty<T>(it : Iterator<T>) : Bool
-    return !it.hasNext();
-
-  public static function filter<TItem>(it : Iterator<TItem>, predicate : TItem -> Bool) : Array<TItem>
-    return reduce(it, function(acc : Array<TItem>, item) {
-        if(predicate(item))
-          acc.push(item);
-        return acc;
-      }, []);
-
-  public static function isIterator(v : Dynamic) {
-    var fields = Types.isAnonymousObject(v) ? Reflect.fields(v) : Type.getInstanceFields(Type.getClass(v));
-    if(!Lambda.has(fields, "next") || !Lambda.has(fields, "hasNext")) return false;
-    return Reflect.isFunction(Reflect.field(v, "next")) && Reflect.isFunction(Reflect.field(v, "hasNext"));
+  public static function toArray<T>(it : Iterator<T>) : Array<T> {
+    var items = [];
+    for(item in it)
+      items.push(item);
+    return items;
   }
+
 }
