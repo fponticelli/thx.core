e61315501c07f6fcf621cb1095044a4b0f7030fb
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-27 11:29:31 -0600

    minor

--- src/thx/core/Maps.hx
+++ src/thx/core/Maps.hx
@@ -5,12 +5,12 @@ import thx.core.Tuple;
 using thx.core.Iterators;
 
 /**
- * Helper methods for Maps
- */
+Helper methods for Maps
+**/
 class Maps {
-  /**
-   * Converts a Map<TKey, TValue> into an Array<Tuple2<TKey, TValue>>
-   */
+/**
+Converts a Map<TKey, TValue> into an Array<Tuple2<TKey, TValue>>
+**/
   public static function tuples<TKey, TValue>(map: IMap<TKey, TValue>): Array<Tuple2<TKey, TValue>>
     return map.keys().map(function(key)
         return new Tuple2(key, map.get(key))
