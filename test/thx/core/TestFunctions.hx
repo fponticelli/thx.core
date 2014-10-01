8096d86f66df309f48c0848619219132816cd9af
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-30 20:45:09 -0600

    fixed 'after' and added 'memoize'

--- test/thx/core/TestFunctions.hx
+++ test/thx/core/TestFunctions.hx
@@ -5,4 +5,49 @@ using thx.core.Functions;
 
 class TestFunctions {
   public function new() { }
+
+  public function testAfter() {
+    var counter = 0,
+        test    = function() Assert.equals(1, ++counter),
+        manage  = Function0.after(3, test);
+
+    Assert.equals(0, counter);
+    manage();
+    Assert.equals(0, counter);
+    manage();
+    Assert.equals(0, counter);
+    manage();
+    Assert.equals(1, counter);
+    manage();
+    Assert.equals(1, counter);
+  }
+
+  public function testOnce() {
+    var counter = 0,
+        test    = function() Assert.equals(1, ++counter),
+        manage  = Function0.once(test);
+
+    Assert.equals(0, counter);
+    manage();
+    Assert.equals(1, counter);
+    manage();
+    Assert.equals(1, counter);
+  }
+
+  public function testMemoize() {
+    var counter = 0,
+        test    = function(x) {
+          ++counter;
+          return x * 10;
+        },
+        manage  = Function1.memoize(test);
+
+    Assert.equals(0, counter);
+    Assert.equals(10, manage(1));
+    Assert.equals(1, counter);
+    Assert.equals(10, manage(1));
+    Assert.equals(1, counter);
+    Assert.equals(20, manage(2));
+    Assert.equals(2, counter);
+  }
 }
