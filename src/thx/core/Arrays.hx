18cb9f45b0a0f4e930b1e86044918dec48f96a0e
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-10-09 23:57:57 -0600

    polyfill for reduce

--- src/thx/core/Arrays.hx
+++ src/thx/core/Arrays.hx
@@ -583,6 +583,44 @@ Unzip an array of Tuple5<T1, T2, T3, T4, T5> to a Tuple5<Array<T1>, Array<T2>, A
     });
     return new Tuple5(a1, a2, a3, a4, a5);
   }
+
+#if js
+  static function __init__() {
+    untyped __js__("
+      // Production steps of ECMA-262, Edition 5, 15.4.4.21
+      // Reference: http://es5.github.io/#x15.4.4.21
+      if (!Array.prototype.reduce) {
+        Array.prototype.reduce = function(callback /*, initialValue*/) {
+          'use strict';
+          if (this == null) {
+            throw new TypeError('Array.prototype.reduce called on null or undefined');
+          }
+          if (typeof callback !== 'function') {
+            throw new TypeError(callback + ' is not a function');
+          }
+          var t = Object(this), len = t.length >>> 0, k = 0, value;
+          if (arguments.length == 2) {
+            value = arguments[1];
+          } else {
+            while (k < len && ! k in t) {
+              k++;
+            }
+            if (k >= len) {
+              throw new TypeError('Reduce of empty array with no initial value');
+            }
+            value = t[k++];
+          }
+          for (; k < len; k++) {
+            if (k in t) {
+              value = callback(value, t[k], k, t);
+            }
+          }
+          return value;
+        };
+      }
+    ");
+  }
+#end
 }
 
 /**
