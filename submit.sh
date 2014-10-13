6bd94c2c4800b52a3de0792615256394071fbfc2
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-10-13 15:01:47 -0600

    release

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm thx.core.zip
-zip -r thx.core.zip hxml src test extraParams.hxml haxelib.json LICENSE README.md
+zip -r thx.core.zip hxml src doc/ImportCore.hx test extraParams.hxml haxelib.json LICENSE README.md
 haxelib submit thx.core.zip
