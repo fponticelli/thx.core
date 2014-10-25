2b248c31b0807bcc0ba361dc7ff61e005ebffbb5
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-10-25 00:01:03 -0600

    simplified implementation

--- src/thx/core/Defaults.hx
+++ src/thx/core/Defaults.hx
@@ -14,71 +14,6 @@ import thx.core.Ints;
 **/
 class Defaults {
 /**
-The method can provide an alternative value `alt` in case `value` is `null`.
-
-```haxe
-var s : String = null;
-trace(s.or('b')); // prints 'b'
-s = 'a';
-trace(s.or('b')); // prints 'a'
-```
-
-Notice that the subject `value` must be a constant identifier (eg: fields, local variables, ...).
-**/
-  macro public static function or<T>(value : ExprOf<Null<T>>, alt : ExprOf<T>) {
-    var ids  = [],
-        salt = ExprTools.toString(alt);
-    function traverse(e : haxe.macro.Type.TypedExpr) switch e.expr {
-      case TArray(a, e):
-        traverse(a);
-        traverse(e);
-      case TConst(TThis):
-        ids.push('this');
-      case TConst(TInt(index)):
-        ids.push('$index');
-      case TLocal(o):
-        ids.push(o.name);
-      case TField(f, v):
-        traverse(f);
-        switch v {
-          case FAnon(id):
-            ids.push(id.toString());
-          case FInstance(_, n):
-            ids.push(n.toString());
-          case _:
-            throw 'invalid expression $e';
-        }
-      case TParenthesis(p):
-        traverse(p);
-      case TCall(e, el):
-        traverse(e);
-        var a = el.map(TypedExprTools.toString.bind(_, true)).join(", ");
-        ids[ids.length - 1] += '($a)';
-      case TBlock(_):
-        ids.push(TypedExprTools.toString(e, true));
-      case _:
-        throw 'invalid expression $e';
-    }
-
-    traverse(Context.typeExpr(value));
-    var first = ids.shift(),
-        temps = ['_0 = $first'].concat(Arrays.mapi(ids, function(_, i) return '_${i+1}')).join(', '),
-        buf   = '{\n  function _() return $salt;\n  var ${temps};\n  if(null == _0) _();\n',
-        path;
-    for(i in 0...ids.length) {
-      var id = ids[i];
-      if(Ints.canParse(id)) {
-        path = '[$id]';
-      } else {
-        path = '.$id';
-      }
-      buf += '  else if(null == (_${i+1} = _$i$path)) _();\n';
-    }
-    buf += '  else _${ids.length};\n}';
-    return Context.parse(buf , value.pos);
-  }
-
-/**
 It traverses a chain of dot/array identifiers and it returns the last value in the chain or null if any of the identifiers is not set.
 
 ```haxe
@@ -89,8 +24,6 @@ trace((o.a.b.c).opt()); // prints 'A'
 ```
 **/
   #if !macro macro #end public static function opt(value : Expr) {
-    // `opt` implementation could reuse `or` but Macro seem to choke
-    // on some nested TBlock/TCall generating erroneous code
     var ids  = [];
     function traverse(e : haxe.macro.Type.TypedExpr) switch e.expr {
       case TArray(a, e):
@@ -115,6 +48,7 @@ trace((o.a.b.c).opt()); // prints 'A'
       case TParenthesis(p):
         traverse(p);
       case TCall(e, el):
+        // TODO, maybe add error here for Python users
         traverse(e);
         var a = el.map(TypedExprTools.toString.bind(_, true)).join(", ");
         ids[ids.length - 1] += '($a)';
@@ -142,13 +76,24 @@ trace((o.a.b.c).opt()); // prints 'A'
     return Context.parse(buf , value.pos);
   }
 
-  macro public static function isNull(value : Expr) {
-    var e = Defaults.opt(value);
-    return macro ($e{e} == null);
-  }
+/**
+The method can provide an alternative value `alt` in case `value` is `null`.
 
-  macro public static function notNull(value : Expr) {
-    var e = Defaults.opt(value);
-    return macro ($e{e} != null);
-  }
+```haxe
+var s : String = null;
+trace(s.or('b')); // prints 'b'
+s = 'a';
+trace(s.or('b')); // prints 'a'
+```
+
+Notice that the subject `value` must be a constant identifier (eg: fields, local variables, ...).
+**/
+  macro public static function or<T>(value : ExprOf<Null<T>>, alt : ExprOf<T>)
+    return macro { var t = $e{Defaults.opt(value)}; t != null ? t : $e{alt}; };
+
+  macro public static function isNull(value : Expr)
+    return macro ($e{Defaults.opt(value)} == null);
+
+  macro public static function notNull(value : Expr)
+    return macro ($e{Defaults.opt(value)} != null);
 }
