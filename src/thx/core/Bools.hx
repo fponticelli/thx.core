b8a4ff60a90bb8232950a06f7307102bc443db0f
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-07 20:24:52 -0700

    more permissive Bool parsing

--- src/thx/core/Bools.hx
+++ src/thx/core/Bools.hx
@@ -17,16 +17,16 @@ Converts a boolean to an integer value (`true` => `1`, `false` => `0`).
 Returns `true` if the passed value is either `true` or `false` (case insensitive).
 */
   public static function canParse(v : String) return switch v.toLowerCase() {
-    case "true", "false": true;
+    case "true", "false", null, "0", "1", "on", "off": true;
     case _: false;
   }
 
 /**
 Returns `true`/`false` if the passed value is `true`/`false` (case insensitive); with any other value it will return null.
 */
-  public static function parse(v : String) return switch v.toLowerCase() {
-    case "true": true;
-    case "false": false;
-    case _: null;
+  public static function parse(v : String) : Bool return switch v.toLowerCase() {
+    case "true", "1", "on": true;
+    case "false", null, "0", "off": false;
+    case v: 'throw unable to parse "$v"';
   }
 }
