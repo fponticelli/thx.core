673caad4696458aa4e4d39f63d5cc6a23e4eb728
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-14 17:24:26 -0600

    minor

--- src/thx/core/QueryString.hx
+++ src/thx/core/QueryString.hx
@@ -1,11 +1,16 @@
 package thx.core;
 
 abstract QueryString(Map<String>) {
+  public static var separator = ";";
+
   @:from public static function parse(s : String) : QueryString {
 
   }
 
-  @:to public function toString() {
+  public function withSeparator(sep : String) {
 
   }
+
+  @:to inline public function toString()
+    return withSeparator(separator);
 }
