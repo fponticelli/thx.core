5891411e50845c39f3de38dc969882fd95aae6c6
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-05-11 16:50:50 -0600

    Add tests for find and findlast

--- test/thx/TestArrays.hx
+++ test/thx/TestArrays.hx
@@ -53,6 +53,14 @@ class TestArrays {
     Assert.same([1,3,5], [1,2,3,4,5,6].filterPluck(_ % 2 != 0));
   }
 
+  public function testFind() {
+    Assert.equals(3, [1,3,5,7,9].find(function(item) return item % 3 == 0));
+  }
+
+  public function testFindLast() {
+    Assert.equals(9, [1,3,5,7,9].findLast(function(item) return item % 3 == 0));
+  }
+
   public function testPluck() {
     Assert.same([2,4,8], [1,2,4].pluck(_ * 2));
   }
