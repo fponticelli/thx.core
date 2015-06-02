9f6e8d9704dc00b58b78cca34a236473fda5d5ff
francescoagati <franescoagati1975@gmail.com>
2015-06-02 23:14:43 +0200

    refactoring fn with multidispatch

--- src/thx/macro/lambda/Functions.hx
+++ src/thx/macro/lambda/Functions.hx
@@ -12,37 +12,49 @@ package thx.macro.lambda;
 
 
 class Functions {
+
+
+
   macro public static function fn(filter:Expr) {
-    var new_filter = filter.map(replace_());
-    return macro function(__tmp1) { return $new_filter;  };
+    var new_filter = replaceWildCard(filter);
+    var occurrences = countMaxWildcard(filter);
+    trace(occurrences);
+    return switch occurrences {
+      case 0:macro function(__tmp0) { return $new_filter;  };
+      case 1:macro function(__tmp0,__tmp1) { return $new_filter;  };
+      case 2:macro function(__tmp0,__tmp1,__tmp2) { return $new_filter;  };
+      case 3:macro function(__tmp0,__tmp1,__tmp2,__tmp3) { return $new_filter;  };
+      case 4:macro function(__tmp0,__tmp1,__tmp2,__tmp3,__tmp4) { return $new_filter;  };
+      default: macro null;
+    };
   }
 
   macro public static function fn0(filter:Expr) return macro function() { return $filter;  };
 
   macro public static function fn1(filter:Expr) {
-    var new_filter = filter.map(replace1());
-    return macro function(__tmp1) { return $new_filter;  };
+    var new_filter = filter.map(replace0());
+    return macro function(__tmp0) { return $new_filter;  };
   }
 
 
   macro public static function fn2(filter:Expr) {
-    var new_filter = filter.map(replace1()).map(replace2());
-    return macro function(__tmp1,__tmp2) { return $new_filter;  };
+    var new_filter = filter.map(replace0()).map(replace1());
+    return macro function(__tmp0,__tmp1) { return $new_filter;  };
   }
 
   macro public static function fn3(filter:Expr) {
-    var new_filter = filter.map(replace1()).map(replace2()).map(replace3());
-    return macro function(__tmp1,__tmp2,__tmp3) { return $new_filter;  };
+    var new_filter = filter.map(replace0()).map(replace1()).map(replace2());
+    return macro function(__tmp0,__tmp1,__tmp2) { return $new_filter;  };
   }
 
   macro public static function fn4(filter:Expr) {
-    var new_filter = filter.map(replace1()).map(replace2()).map(replace3()).map(replace4());
-    return macro function(__tmp1,__tmp2,__tmp3,__tmp4) { return $new_filter;  };
+    var new_filter = filter.map(replace0()).map(replace1()).map(replace2()).map(replace3());
+    return macro function(__tmp0,__tmp1,__tmp2,__tmp3) { return $new_filter;  };
   }
 
   macro public static function fn5(filter:Expr) {
-    var new_filter = filter.map(replace1()).map(replace2()).map(replace3()).map(replace4()).map(replace5());
-    return macro function(__tmp1,__tmp2,__tmp3,__tmp4,__tmp5) { return $new_filter;  };
+    var new_filter = filter.map(replace0()).map(replace1()).map(replace2()).map(replace3()).map(replace4());
+    return macro function(__tmp0,__tmp1,__tmp2,__tmp3,__tmp4) { return $new_filter;  };
   }
 
 }
--- test/thx/TestLambda.hx
+++ test/thx/TestLambda.hx
@@ -9,12 +9,19 @@ class TestLambda {
   public function testFunctions() {
     Assert.same([2,3], [1,2].map(fn(_+1)));
     Assert.equals(0, fn0((0))());
-    Assert.equals(2, fn1((_1))(2));
-    Assert.equals(3, fn2(_1+_2)(1,2));
-    Assert.equals(6, fn3(_1+_2+_3)(1,2,3));
-    Assert.equals(10, fn4(_1+_2+_3+_4)(1,2,3,4));
-    Assert.equals(15, fn5(_1+_2+_3+_4+_5)(1,2,3,4,5));
-    Assert.equals(6, fn5(_1+_2+_3)(1,2,3,4,5));
+    Assert.equals(2, fn1((_0))(2));
+    Assert.equals(3, fn2(_0+_1)(1,2));
+    Assert.equals(6, fn3(_0+_1+_2)(1,2,3));
+    Assert.equals(10, fn4(_0+_1+_2+_3)(1,2,3,4));
+    Assert.equals(15, fn5(_0+_1+_2+_3+_4)(1,2,3,4,5));
+    Assert.equals(6, fn5(_0+_1+_2)(1,2,3,4,5));
+
+    Assert.equals(1, fn(_0)(1));
+    Assert.equals(3, fn(_0+_1)(1,2));
+    Assert.equals(6, fn(_0+_1+_2)(1,2,3));
+    Assert.equals(10, fn(_0+_1+_2+_3)(1,2,3,4));
+    Assert.equals(15, fn(_0+_1+_2+_3+_4)(1,2,3,4,5));
+
   }
 
   public function testForms() {
@@ -58,7 +65,7 @@ class TestLambda {
 
       _.equals(false,not(true));
       _.equals(true,not(false));
-      
+
     });
   }
 
