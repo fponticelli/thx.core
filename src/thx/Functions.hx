f173d30da265919178ec7b819dc26b4108a864db
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-06-09 16:52:34 -0600

    moved SlambdaMacro to its own module

--- src/thx/Functions.hx
+++ src/thx/Functions.hx
@@ -2,11 +2,8 @@ package thx;
 
 #if macro
 import haxe.macro.Expr;
-import haxe.macro.ExprTools;
-using haxe.macro.ExprTools;
+import thx.macro.lambda.SlambdaMacro;
 #end
-
-
 
 /**
 Extension methods for functions with arity 0 (functions that do not take arguments).
@@ -143,7 +140,7 @@ Returns a function that behaves the same as `callback` but has its arguments inv
 /**
 Lambda expressions
 **/
-  public macro static function fn<T, T2>(fn : ExprOf<T -> T2>, restArgs : Array<Expr>) 
+  public macro static function fn<T, T2>(fn : ExprOf<T -> T2>, restArgs : Array<Expr>)
     return SlambdaMacro.f(fn, restArgs);
 }
 
@@ -181,8 +178,8 @@ Wraps `callback` in a function that negates its results.
 /**
 Lambda expressions
 **/
-  public macro static function fn<T, T2, T3>(fn : ExprOf<T -> T2 -> T3>, restArgs : Array<Expr>) 
-    return SlambdaMacro.f(fn, restArgs);      
+  public macro static function fn<T, T2, T3>(fn : ExprOf<T -> T2 -> T3>, restArgs : Array<Expr>)
+    return SlambdaMacro.f(fn, restArgs);
 }
 
 /**
@@ -219,7 +216,7 @@ Wraps `callback` in a function that negates its results.
 /**
 Lambda expressions
 **/
-  public macro static function fn<T, T2, T3, T4>(fn : ExprOf<T -> T2 -> T3 -> T4>, restArgs : Array<Expr>) 
+  public macro static function fn<T, T2, T3, T4>(fn : ExprOf<T -> T2 -> T3 -> T4>, restArgs : Array<Expr>)
     return SlambdaMacro.f(fn, restArgs);
 }
 
@@ -258,79 +255,6 @@ The `identity` function returns the value of its argument.
 /**
 Lambda expressions
 **/
-  public macro static function fn<T>(fn : ExprOf<Void -> T>, restArgs : Array<Expr>) 
+  public macro static function fn<T>(fn : ExprOf<Void -> T>, restArgs : Array<Expr>)
     return SlambdaMacro.f(fn, restArgs);
 }
-
-#if macro
-/** 
-Lambda expressions. Standalone version: https://github.com/ciscoheat/slambda
-**/
-private class SlambdaMacro {
-  
-  public static function f(fn : Expr, restArgs : Array<Expr>) {
-    // If called through a static extension, fn contains the special "@:this this" expression:
-    // http://haxe.org/manual/macro-limitations-static-extension.html
-    var isExtension = fn.expr.match(EMeta({ name: ":this", params: _, pos: _}, {expr: EConst(CIdent("this")), pos: _}));
-
-    if (isExtension) return {expr: ECall(fn, restArgs.map(createLambdaExpression.bind(true))), pos: fn.pos};
-    
-    // If not an extension, return only fn. Rest arguments won't make sense here.
-    return restArgs.length == 0
-      ? createLambdaExpression(false, fn)
-      : untyped Context.error('Rest arguments can only be used in static extensions.', restArgs[restArgs.length - 1].pos);
-  }
-
-  static var underscoreParam = ~/^_\d*$/;
-  static function createLambdaExpression(isExtension : Bool, e : Expr) : Expr {
-
-    // If no arrow syntax, detect underscore parameters.
-    switch e.expr {
-      case EBinop(OpArrow, _, _):
-      case _: 
-        var params = new Map<String, Expr>();
-        function findParams(e2 : Expr) {
-          switch e2.expr {
-            case EConst(CIdent(v)) if (underscoreParam.match(v)):
-              params.set(v, e2);
-            case _:
-              e2.iter(findParams);
-          }
-        }
-        findParams(e);
-
-        var paramArray = [for (p in params) p];
-        if (paramArray.length > 0) {
-          // If underscore parameters found, create an arrow syntax of the expression.
-          // Sorted so the parameters are in the correct order in the function definition.
-          paramArray.sort(function(x, y) return x.toString() > y.toString() ? 1 : 0);
-          e = macro $a{paramArray} => $e;
-        }
-    }
-
-    return switch e.expr {
-      case EBinop(OpArrow, e1, e2):
-        // Extract lambda arguments [a,b] => ...
-        var lambdaArgs = switch e1.expr {
-          case EConst(CIdent(v)): [v];
-          case EArrayDecl(values) if(values.length > 0): [for (v in values) v.toString()];
-          case _: untyped Context.error("Invalid lambda argument, use x => ... or [x,y] => ...", e1.pos);
-        }
-        
-        {
-          expr: EFunction(null, {
-            ret: null,
-            params: [],
-            expr: macro return $e2,
-            args: [for(arg in lambdaArgs) { name: arg, type: null, opt: false }]
-          }),
-          pos: e.pos
-        };
-
-      // If not an extension, it should return a non-lambda expression as a Void -> T function
-      // to stay consistent with fn("x") <-> function() return "x".
-      case _: isExtension ? e : macro function() return $e;
-    }
-  }
-}
-#end
---
+++ src/thx/macro/lambda/SlambdaMacro.hx
@@ -0,0 +1,77 @@
+package thx.macro.lambda;
+
+import haxe.macro.Expr;
+import haxe.macro.ExprTools;
+using haxe.macro.ExprTools;
+
+/**
+Lambda expressions. Standalone version: https://github.com/ciscoheat/slambda
+**/
+class SlambdaMacro {
+
+  public static function f(fn : Expr, restArgs : Array<Expr>) {
+    // If called through a static extension, fn contains the special "@:this this" expression:
+    // http://haxe.org/manual/macro-limitations-static-extension.html
+    var isExtension = fn.expr.match(EMeta({ name: ":this", params: _, pos: _}, {expr: EConst(CIdent("this")), pos: _}));
+
+    if (isExtension) return {expr: ECall(fn, restArgs.map(createLambdaExpression.bind(true))), pos: fn.pos};
+
+    // If not an extension, return only fn. Rest arguments won't make sense here.
+    return restArgs.length == 0
+      ? createLambdaExpression(false, fn)
+      : untyped Context.error('Rest arguments can only be used in static extensions.', restArgs[restArgs.length - 1].pos);
+  }
+
+  static var underscoreParam = ~/^_\d*$/;
+  static function createLambdaExpression(isExtension : Bool, e : Expr) : Expr {
+
+    // If no arrow syntax, detect underscore parameters.
+    switch e.expr {
+      case EBinop(OpArrow, _, _):
+      case _:
+        var params = new Map<String, Expr>();
+        function findParams(e2 : Expr) {
+          switch e2.expr {
+            case EConst(CIdent(v)) if (underscoreParam.match(v)):
+              params.set(v, e2);
+            case other:
+              trace(other);
+              e2.iter(findParams);
+          }
+        }
+        findParams(e);
+
+        var paramArray = [for (p in params) p];
+        if (paramArray.length > 0) {
+          // If underscore parameters found, create an arrow syntax of the expression.
+          // Sorted so the parameters are in the correct order in the function definition.
+          paramArray.sort(function(x, y) return x.toString() > y.toString() ? 1 : 0);
+          e = macro $a{paramArray} => $e;
+        }
+    }
+
+    return switch e.expr {
+      case EBinop(OpArrow, e1, e2):
+        // Extract lambda arguments [a,b] => ...
+        var lambdaArgs = switch e1.expr {
+          case EConst(CIdent(v)): [v];
+          case EArrayDecl(values) if(values.length > 0): [for (v in values) v.toString()];
+          case _: untyped Context.error("Invalid lambda argument, use x => ... or [x,y] => ...", e1.pos);
+        }
+
+        {
+          expr: EFunction(null, {
+            ret: null,
+            params: [],
+            expr: macro return $e2,
+            args: [for(arg in lambdaArgs) { name: arg, type: null, opt: false }]
+          }),
+          pos: e.pos
+        };
+
+      // If not an extension, it should return a non-lambda expression as a Void -> T function
+      // to stay consistent with fn("x") <-> function() return "x".
+      case _: isExtension ? e : macro function() return $e;
+    }
+  }
+}
