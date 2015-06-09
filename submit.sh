4635f54e23e1417a143f2aa99252f5c611cda562
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-06-08 20:15:36 -0600

    release

--- submit.sh
+++ submit.sh
@@ -3,6 +3,6 @@ set -e
 cd doc
 haxe build_doc.hxml || { cd ..; echo "build_doc failed"; exit 1; }
 cd ..
-rm thx.core.zip
+rm -f thx.core.zip
 zip -r thx.core.zip src doc/ImportCore.hx test extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
 haxelib submit thx.core.zip
