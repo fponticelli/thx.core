2fef2a1ee845ac82d2280ae8df2cf70ed2351ad6
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-06 22:37:26 -0600

    more assert methods

--- src/thx/Assert.hx
+++ src/thx/Assert.hx
@@ -8,6 +8,38 @@ class Assert {
   #end
 
 /**
+Checks that the test value matches at least one of the possibilities.
+@param possibility: An array of possible matches
+@param value: The value to test
+@param msg: An optional error message. If not passed a default one will be used
+@param pos: Code position where the Assert call has been executed. Don't fill it
+unless you know what you are doing.
+*/
+  public static function containedIn<T>(possibilities : Array<T>, value : T, ?msg : String , ?pos : PosInfos) {
+    if(Arrays.contains(possibilities, value)) {
+      isTrue(true, msg, pos);
+    } else {
+      fail(msg == null ? "value " + q(value) + " not found in the expected possibilities " + possibilities : msg, pos);
+    }
+  }
+
+/**
+Checks that the test array contains the match parameter.
+@param match: The element that must be included in the tested array
+@param values: The values to test
+@param msg: An optional error message. If not passed a default one will be used
+@param pos: Code position where the Assert call has been executed. Don't fill it
+unless you know what you are doing.
+*/
+  public static function contains<T>(match : T, values : Array<T>, ?msg : String , ?pos : PosInfos) {
+    if(Arrays.contains(values, match)) {
+      isTrue(true, msg, pos);
+    } else {
+      fail(msg == null ? "values " + q(values) + " do not contain "+match: msg, pos);
+    }
+  }
+
+/**
 Asserts successfully when the value parameter is equal to the expected one.
 ```haxe
 Assert.equals(10, age);
@@ -24,6 +56,49 @@ unless you know what you are doing.
     if(msg == null) msg = "expected " + Strings.quote(expected) + " but was " + Strings.quote(value);
     isTrue(expected == value, msg, pos);
     #end
+  }
+
+/**
+Checks that the test array does not contain the match parameter.
+@param match: The element that must NOT be included in the tested array
+@param values: The values to test
+@param msg: An optional error message. If not passed a default one will be used
+@param pos: Code position where the Assert call has been executed. Don't fill it
+unless you know what you are doing.
+*/
+  public static function excludes<T>(match : T, values : Array<T>, ?msg : String , ?pos : PosInfos) {
+    if(!Array.contains(values, match)) {
+      isTrue(true, msg, pos);
+    } else {
+      fail(msg == null ? "values " + q(values) + " do contain "+match: msg, pos);
+    }
+  }
+
+/**
+Forces a failure.
+@param msg: An optional error message. If not passed a default one will be used
+@param pos: Code position where the Assert call has been executed. Don't fill it
+unless you know what you are doing.
+*/
+  public static function fail(msg = "failure expected", ?pos : PosInfos)
+    isTrue(false, msg, pos);
+
+/**
+Same as Assert.equals but considering an approximation error.
+```haxe
+Assert.floatEquals(Math.PI, value);
+```
+@param expected: The expected value to check against
+@param value: The value to test
+@param approx: The approximation tollerance. Default is 1e-5
+@param msg: An optional error message. If not passed a default one will be used
+@param pos: Code position where the Assert call has been executed. Don't fill it
+unless you know what you are doing.
+@todo test the approximation argument
+*/
+  public static function floatEquals(expected : Float, value : Float, ?approx : Float, ?msg : String , ?pos : PosInfos) : Void {
+    if (msg == null) msg = "expected " + q(expected) + " but was " + q(value);
+    return isTrue(_floatEquals(expected, value, approx), msg, pos);
   }
 
 /**
@@ -102,9 +177,12 @@ Assert.match(~/x/i, "haXe");
 @param pos: Code position where the Assert call has been executed. Don't fill it
 unless you know what you are doing.
 */
+  #if no_asserts inline #end
   public static function match(pattern : EReg, value : Dynamic, ?msg : String , ?pos : PosInfos) {
+    #if !no_asserts
     if(msg == null) msg = "the value " + Strings.quote(value) + "does not match the provided pattern";
     isTrue(pattern.match(value), msg, pos);
+    #end
   }
 
 /**
@@ -141,6 +219,114 @@ unless you know what you are doing.
     isTrue(value != null, msg, pos);
     #end
   }
+
+/**
+Adds a successful assertion for cases where there are no values to assert.
+@param msg: An optional success message. If not passed a default one will be used
+@param pos: Code position where the Assert call has been executed. Don't fill it
+unless you know what you are doing.
+*/
+  public static function pass(msg = "pass expected", ?pos : PosInfos) {
+    isTrue(true, msg, pos);
+  }
+
+/**
+It is used to test an application that under certain circumstances must
+react throwing an error. This assert guarantees that the error is of the
+correct type (or Dynamic if non is specified).
+```haxe
+Assert.raises(function() { throw "Error!"; }, String);
+```
+@param method: A method that generates the exception.
+@param type: The type of the expected error. Defaults to Dynamic (catch all).
+@param msgNotThrown: An optional error message used when the function fails to raise the expected
+     exception. If not passed a default one will be used
+@param msgWrongType: An optional error message used when the function raises the exception but it is
+     of a different type than the one expected. If not passed a default one will be used
+@param pos: Code position where the Assert call has been executed. Don't fill it
+unless you know what you are doing.
+@todo test the optional type parameter
+*/
+  public static function raises(method:Void -> Void, ?type : Dynamic, ?msgNotThrown : String , ?msgWrongType : String, ?pos : PosInfos) {
+    try {
+      method();
+      if (null == msgNotThrown) {
+        var name = Types.toString(type);
+        msgNotThrown = "exception of type " + name + " not raised";
+      }
+      fail(msgNotThrown, pos);
+    } catch (ex : Dynamic) {
+      if (null == msgWrongType) {
+        var name = Types.toString(type);
+        msgWrongType = "expected throw of type " + name + " but was "  + ex;
+      }
+      if(null == type) {
+        pass(pos);
+      } else {
+        isTrue(Std.is(ex, type), msgWrongType, pos);
+      }
+    }
+  }
+
+/**
+Checks that the expected values is contained in value.
+@param match: the string value that must be contained in value
+@param value: the value to test
+@param msg: An optional error message. If not passed a default one is be used
+@param pos: Code position where the Assert call has been executed.
+*/
+  public static function stringContains(match : String, value : String, ?msg : String , ?pos : PosInfos) {
+    if (value != null && value.indexOf(match) >= 0) {
+      isTrue(true, msg, pos);
+    } else {
+      fail(msg == null ? "value " + q(value) + " does not contain " + q(match) : msg, pos);
+    }
+  }
+
+/**
+Checks that the test string contains all the values in `sequence` in the order
+they are defined.
+@param sequence: the values to match in the string
+@param value: the value to test
+@param msg: An optional error message. If not passed a default one is be used
+@param pos: Code position where the Assert call has been executed.
+*/
+  public static function stringSequence(sequence : Array<String>, value : String, ?msg : String , ?pos : PosInfos) {
+    if (null == value) {
+      fail(msg == null ? "null argument value" : msg, pos);
+      return;
+    }
+    var p = 0;
+    for (s in sequence) {
+      var p2 = value.indexOf(s, p);
+      if (p2 < 0) {
+        if (msg == null)
+        {
+          msg = "expected '" + s + "' after ";
+          if (p > 0) {
+            var cut = value.substr(0, p);
+            if (cut.length > 30)
+              cut = '...' + cut.substr( -27);
+            msg += " '" + cut + "'" ;
+          } else
+            msg += " begin";
+        }
+        fail(msg, pos);
+        return;
+      }
+      p = p2 + s.length;
+    }
+    isTrue(true, msg, pos);
+  }
+
+/**
+Creates a warning message.
+@param msg: A mandatory message that justifies the warning.
+@param pos: Code position where the Assert call has been executed. Don't fill it
+unless you know what you are doing.
+*/
+  public static function warn(msg)
+    results.add(Warning(msg));
 }
 
 interface IAssertBehavior {
