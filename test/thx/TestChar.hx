95e4ac482047f4c1ede030b16c11587848fad54b
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-14 21:59:54 -0600

    add more tests

--- test/thx/TestChar.hx
+++ test/thx/TestChar.hx
@@ -10,12 +10,22 @@ class TestChar {
   public function testChar() {
     var char : Char = 120;
 
-    Assert.equals(char, 120);
-    Assert.equals(char.toString(), "x");
+    Assert.equals(120, char);
+    Assert.equals("x", char.toString());
 
     char = "y";
 
-    Assert.equals(char, 121);
-    Assert.equals(char.toString(), "y");
+    Assert.equals(121, char);
+    Assert.equals("y", char.toString());
+
+    Assert.equals(89, char.toUpperCase());
+    Assert.equals("Y", char.toUpperCase().toString());
+
+    char = char.next();
+
+    Assert.equals(122, char);
+    Assert.equals("z", char.toString());
+
+    Assert.equals(121, char.prev());
   }
 }
