8bb2f2c3b78549e9bb4174a74675ab1e18c0a1df
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-14 21:40:04 -0600

    added basic tests

--- test/thx/TestChar.hx
+++ test/thx/TestChar.hx
@@ -8,6 +8,14 @@ class TestChar {
   public function new() { }
 
   public function testChar() {
-    
+    var char : Char = 120;
+
+    Assert.equals(char, 120);
+    Assert.equals(char.toString(), "x");
+
+    char = "y";
+
+    Assert.equals(char, 121);
+    Assert.equals(char.toString(), "y");
   }
 }
