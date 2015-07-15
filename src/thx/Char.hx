87845a588eddf9b00c9fb669a4bd4ba26d50cf81
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-14 21:47:19 -0600

    added Char equality operators

--- src/thx/Char.hx
+++ src/thx/Char.hx
@@ -1,8 +1,28 @@
 package thx;
 
+import haxe.Utf8;
+
 abstract Char(Int) from Int to Int  {
   @:from inline public static function fromString(s : String) : Char
-    return haxe.Utf8.charCodeAt(s, 0);
+    return Utf8.charCodeAt(s, 0);
+
+  public function compare(other : Char)
+    return Utf8.compare(toString(), other);
+
+  @:op(A==B) inline public function equals(other : Char)
+    return compare(other) == 0;
+
+  @:op(A>B) inline public function greater(other : Char)
+    return compare(other) > 0;
+
+  @:op(A>=B) inline public function greaterEquals(other : Char)
+    return compare(other) >= 0;
+
+  @:op(A<=B) inline public function lessEquals(other : Char)
+    return compare(other) <= 0;
+
+  @:op(A<B) inline public function less(other : Char)
+    return compare(other) < 0;
 
   @:to inline public function toString() : String
     return String.fromCharCode(this);
