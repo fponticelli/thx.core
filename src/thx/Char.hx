7dd9d26f853b8df23cd859e507b0b3252e8dc9e0
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-14 21:42:03 -0600

    implementations for Char

--- src/thx/Char.hx
+++ src/thx/Char.hx
@@ -1,5 +1,9 @@
 package thx;
 
 abstract Char(Int) from Int to Int  {
-  
+  @:from inline public static function fromString(s : String) : Char
+    return haxe.Utf8.charCodeAt(s, 0);
+
+  @:to inline public function toString() : String
+    return String.fromCharCode(this);
 }
