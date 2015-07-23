4f1e8c9c1e953d40a15da392756cb9ff9557fd08
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-22 21:51:42 -0600

    added test for Iterators.equals

--- test/thx/TestIterators.hx
+++ test/thx/TestIterators.hx
@@ -11,5 +11,13 @@ using thx.Functions;
 class TestIterators {
   public function new() {}
 
-  
+  public function testEquals() {
+    var a = [1,2,3],
+        b = [1,2],
+        c = [1,2,4];
+
+    Assert.isTrue(a.iterator().equals(a.iterator()));
+    Assert.isFalse(a.iterator().equals(b.iterator()));
+    Assert.isFalse(a.iterator().equals(c.iterator()));
+  }
 }
