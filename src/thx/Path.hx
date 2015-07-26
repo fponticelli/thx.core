24cef5e42fc64f507b5aa6de6e7301cbe34a488f
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-25 18:43:16 -0600

    changed to use Array<String> as wrapped type and fixed issues with Java

--- src/thx/Path.hx
+++ src/thx/Path.hx
@@ -4,129 +4,141 @@ using thx.Arrays;
 using thx.Functions;
 using thx.Strings;
 
-abstract Path(PathType) from PathType to PathType {
-  public static var nixSeparator(default, null) : String = "/";
-  public static var win32Separator(default, null) : String = "\\";
+abstract Path(Array<String>) {
+  inline public static var nixSeparator : String = "/";
+  inline public static var win32Separator : String = "\\";
 
-  static var zero = String.fromCharCode(0);
   public static function isValidNix(path : Path) : Bool
-    return path.get_path().any.fn(!_.contains("/") && !_.contains(zero));
+    return path.path.any.fn(!_.contains("/"));
 
-  static var WIN32_CHARS = ~/[<>:"\/\|?*\0]/g;
   public static function isValidWin32(path : Path) : Bool
-    return path.get_path().any.fn(!WIN32_CHARS.match(_));
+    return path.path.any.fn(!(~/[<>:\/\|?*"]/g).match(_));
 
   public static function normalizeNix(path : Path, ?replacement = "_")
-    return path.map.fn(_.replace("/", replacement).replace(zero, ""));
+    return path.map.fn(_.replace("/", replacement));
 
   public static function normalizeWin32(path : Path, ?replacement = "_")
-    return path.map.fn(WIN32_CHARS.replace(_, replacement));
+    return path.map.fn((~/[<>:"\/\|?*]/g).replace(_, replacement));
 
+  public var path(get, never) : Array<String>;
   public var root(get, never) : String;
   public var sep(get, never) : String;
 
-  static var WIN32_ROOT = ~/^([a-z]+[:][\\])/i;
-  @:from public static function fromString(s : String) : Path {
+  @:from
+  public static function fromString(s : String) : Path {
     if(s.contains(win32Separator)) {
-      if(WIN32_ROOT.match(s)) {
-        return new Path(
-          WIN32_ROOT.matched(1),
-          WIN32_ROOT.matchedRight().split(win32Separator),
+      var re = ~/^([a-z]+[:][\\])/i;
+      if(re.match(s)) {
+        return create(
+          re.matched(1),
+          re.matchedRight().split(win32Separator),
           win32Separator);
       } else {
-        return new Path("", s.split(win32Separator), win32Separator);
+        return create("", s.split(win32Separator), win32Separator);
       }
+    } else {
+      return create(
+        s.startsWith(nixSeparator) ? nixSeparator : "",
+        s.split(nixSeparator),
+        nixSeparator);
     }
-    return new Path(
-      s.startsWith(nixSeparator) ? nixSeparator : "",
-      s.split(nixSeparator),
-      nixSeparator);
   }
 
-  function new(root : String, path : Array<String>, sep : String) {
-    path = path.compact().filter.fn(_ != ".");
-    path = path.reduce(function(acc : Array<String>, s : String) {
+  #if java inline #end //WHY????
+  public static function resolve(path : Array<String>, isAbsolute : Bool) {
+    // removes .
+    path = path.compact().filter(function(s) return s != ".");
+    // simplify ..
+    return path.reduce(function(acc : Array<String>, s : String) {
       if(s == ".." && acc.length > 0 && acc.last() != "..") {
         return acc.slice(0, -1);
-      } else if(s == ".." && root != "") {
+      } else if(s == ".." && isAbsolute) {
         return acc;
       } else {
         return acc.concat([s]);
       }
     }, []);
-    this = { root : root, path : path, sep : sep };
   }
 
+  inline public static function create(root : String, path : Array<String>, sep : String) : Path
+    return new Path([sep, root].concat(resolve(path, root != "")));
+
+  inline public static function raw(parts : Array<String>)
+    return new Path(parts);
+
+  inline function new(parts : Array<String>) : Void
+    this = parts;
+
   public function asAbsolute(?root = "C:\\")
-    return new Path(
-      this.sep == nixSeparator ? nixSeparator : root,
-      this.path,
-      this.sep
+    return create(
+      sep == nixSeparator ? nixSeparator : root,
+      path,
+      sep
     );
 
   public function asRelative()
-    return new Path(
+    return create(
       "",
-      this.path,
-      this.sep
+      path,
+      sep
     );
 
   public function normalize()
-    return isWin32() ? normalizeWin32(this) : normalizeNix(this);
+    return isWin32() ? normalizeWin32(get_self()) : normalizeNix(get_self());
 
-  public function isAbsolute()
-    return this.root != "";
+  inline public function isAbsolute()
+    return root != "";
 
-  public function isRelative()
-    return this.root == "";
+  inline public function isRelative()
+    return root == "";
 
   public function isRoot()
-    return isAbsolute() && this.path.length == 0;
+    return isAbsolute() && path.length == 0;
 
-  public function isNix()
+  inline public function isNix()
     return sep == nixSeparator;
 
-  public function isWin32()
+  inline public function isWin32()
     return sep == win32Separator;
 
   public function isValid()
-    return isWin32() ? isValidWin32(this) : isValidNix(this);
+    return isWin32() ? isValidWin32(get_self()) : isValidNix(get_self());
 
   public function base(?end : String) : String {
-    if(this.path.length == 0)
+    if(path.length == 0)
       return '';
-    var name = this.path.last();
+    var name = path.last();
     if(null != end && name.endsWith(end))
       return name.substring(0, name.length - end.length);
     return name;
   }
 
   public function ext() : String {
-    if(this.path.length == 0)
+    if(path.length == 0)
       return '';
-    return this.path.last().afterLast(".");
+    return path.last().afterLast(".");
   }
 
   public function dir() : String
     return up().toString();
 
   public function map(handler : String -> String) : Path
-    return new Path(
-      this.root,
-      this.path.map(handler),
-      this.sep
+    return create(
+      root,
+      path.map(handler),
+      sep
     );
 
   public function pathTo(destination : Path) : Path {
     return switch [isAbsolute(), destination.isAbsolute()] {
-      case [true, true] if(this.root == destination.root):
-        var opath = destination.get_path(),
-            common = this.path.commonsFromStart(opath);
-        return new Path("",
-          this.path.slice(0, this.path.length - common.length)
+      case [true, true] if(root == destination.root):
+        var opath = destination.path,
+            common = path.commonsFromStart(opath);
+        return create("",
+          path.slice(0, path.length - common.length)
             .map(function(_) return '..')
             .concat(opath.slice(common.length)),
-          this.sep);
+          sep);
       case [true, true]   | [false, true]:
         return destination;
       case [false, false] | [true, false]:
@@ -138,47 +150,37 @@ abstract Path(PathType) from PathType to PathType {
     return up().join(path);
 
   public function toNix() : Path
-    return isNix() ? this : {
-      root : isAbsolute() ? nixSeparator : "",
-      path : this.path.copy(),
-      sep  : nixSeparator
-    };
+    return isNix() ?
+      get_self() :
+      new Path([nixSeparator, isAbsolute() ? nixSeparator : ""].concat(path));
 
   public function toWin32(?root : String = "C:\\") : Path
-    return isWin32() ? this : {
-      root : isAbsolute() ? root : "",
-      path : this.path.copy(),
-      sep  : win32Separator
-    };
+    return isWin32() ?
+      get_self() :
+      new Path([win32Separator, isAbsolute() ? root : ""].concat(path));
 
   public function up(?n = 1) : Path
-    return isRoot() ? this : {
-      root : this.root,
-      path : this.path.slice(0, -n),
-      sep  : this.sep
-    };
+    return isRoot() ? get_self() :
+    new Path([sep, root].concat(this.slice(2, -n)));
 
   @:op(A/B) public function join(other : Path) : Path {
     if(other.isAbsolute())
       return other;
-    return new Path(this.root, this.path.concat(other.get_path()), this.sep);
+    return create(root, path.concat(other.path), sep);
   }
 
   @:to public function toString()
-    return !isAbsolute() && this.path.length == 0 ? '.' : this.root + this.path.join(sep);
+    return !isAbsolute() && path.length == 0 ? '.' : root + path.join(sep);
 
-  function get_path() : Array<String>
-    return this.path;
+  inline function get_path() : Array<String>
+    return this.slice(2);
 
-  function get_root() : String
-    return this.root;
+  inline function get_root() : String
+    return this[1];
 
-  function get_sep() : String
-    return this.sep;
-}
+  inline function get_sep() : String
+    return this[0];
 
-typedef PathType = {
-  root : String,
-  path : Array<String>,
-  sep : String
+  inline function get_self() : Path
+    return cast this;
 }
