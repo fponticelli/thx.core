76993c1ea25e05d92e5c473275933e01af14d108
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-30 23:10:57 -0600

    added implementation of localOffset

--- src/thx/DateTime.hx
+++ src/thx/DateTime.hx
@@ -1,8 +1,19 @@
 package thx;
 
 using haxe.Int64;
+using thx.Int64s;
+import thx.DateTimeUtc.*;
 
+@:access(thx.DateTimeUtc)
 abstract DateTime(Array<Int64>) {
+  static public function localOffset() : Time {
+    var now = DateTimeUtc.now(),
+        local = new Date(now.year, now.month - 1, now.day, now.hour, now.minute, now.second),
+        delta = local.getTime() - Math.ffloor(now.toFloat() / 1000) * 1000;
+
+    return new Time(Int64s.fromFloat(delta) * ticksPerMillisecondI64);
+  }
+
   public var utc(get, never) : DateTimeUtc;
   public var offset(get, never) : Time;
 
