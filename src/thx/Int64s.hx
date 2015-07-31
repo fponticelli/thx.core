422e81334316ee900a7f2b57ffa1754361642022
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-30 19:18:28 -0600

    added Int64s.toFloat()

--- src/thx/Int64s.hx
+++ src/thx/Int64s.hx
@@ -75,10 +75,33 @@ class Int64s {
     return current;
   }
 
-  // TODO
-  //public static function toFloat(i : Int64) : Float {
-  //  return 0.0;
-  //}
+  static var min = Int64.make(0x80000000, 0);
+  static var one = Int64.make(0, 1);
+  static var zero = Int64.make(0, 0);
+
+/**
+Converts an `Int64` to `Float`;
+
+Implementation by Elliott Stoneham.
+*/
+  public static function toFloat(i : Int64) : Float {
+    var isNegative = false;
+    if(i < 0) {
+      if(i < min)
+        return -9223372036854775808.0; // most -ve value can't be made +ve
+      isNegative = true;
+      i = -i;
+    }
+    var multiplier = 1.0,
+        ret = 0.0;
+    for(_ in 0...64) {
+      if(i.and(one) != zero)
+        ret += multiplier;
+      multiplier *= 2.0;
+      i = i.shr(1);
+    }
+    return (isNegative ? -1 : 1) * ret;
+  }
 
   public static function fromFloat(f : Float) : Int64 {
     if(Math.isNaN(f) || !Math.isFinite(f))
