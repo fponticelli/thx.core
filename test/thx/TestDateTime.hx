c923b54163ef919b1918ed5709bfff7bed7ad370
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-30 23:33:11 -0600

    better test

--- test/thx/TestDateTime.hx
+++ test/thx/TestDateTime.hx
@@ -9,6 +9,6 @@ class TestDateTime {
   public function testLocalOffset() {
     var ref   = DateHelper.localOffset(),
         delta = DateTime.localOffset();
-    Assert.isTrue(ref == delta);
+    Assert.isTrue(ref == delta, 'expected $ref but got $delta');
   }
 }
