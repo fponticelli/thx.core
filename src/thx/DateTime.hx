ab5ba28e6322863efcb2fa65e00045f008f0acec
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-31 22:41:31 -0600

    added several properties to DateTime

--- src/thx/DateTime.hx
+++ src/thx/DateTime.hx
@@ -36,13 +36,70 @@ abstract DateTime(Array<Int64>) {
   inline function new(parts : Array<Int64>)
     this = parts;
 
+  public var ticks(get, never) : Int64;
+
+  public var year(get, never) : Int;
+  public var month(get, never) : Month;
+  public var day(get, never) : Int;
+
+  public var hour(get, never) : Int;
+  public var minute(get, never) : Int;
+  public var second(get, never) : Int;
+  public var millisecond(get, never) : Int;
+
+  public var dayOfWeek(get, never) : Weekday;
+  public var dayOfYear(get, never) : Int;
+  public var timeOfDay(get, never) : Time;
+
   @:to inline function get_utc() : DateTimeUtc
     return new DateTimeUtc(this[0]);
 
   @:to inline function get_offset() : Time
     return new Time(this[1]);
 
+  inline public function withOffset(offset : Time)
+    return create(utc, offset);
+
+  @:to inline public function toUtc() : DateTimeUtc
+    return utc;
+
+  inline function clockDateTime() : DateTimeUtc
+    return new DateTimeUtc(utc.ticks + offset.ticks);
+
   //1997-07-16T19:20:30+01:00
   public function toString()
     return '$year-${month.lpad(2)}-${day.lpad(2)}T${hour.lpad(2)}:${minute.lpad(2)}:${second.lpad(2)}${millisecond != 0 ? "."+millisecond.lpad(3, "0") : ""}${offset.toGmtString()}';
+
+  @:to inline function get_ticks() : Int64
+    return timeOfDay.ticks;
+
+  inline function get_year() : Int
+    return clockDateTime().year;
+
+  inline function get_month() : Month
+    return clockDateTime().month;
+
+  inline function get_day() : Int
+    return clockDateTime().day;
+
+  inline function get_hour() : Int
+    return clockDateTime().hour;
+
+  inline function get_minute() : Int
+    return clockDateTime().minute;
+
+  inline function get_dayOfWeek() : Weekday
+    return clockDateTime().dayOfWeek;
+
+  inline function get_dayOfYear() : Int
+    return clockDateTime().dayOfYear;
+
+  inline function get_millisecond() : Int
+    return clockDateTime().millisecond;
+
+  inline function get_second() : Int
+    return clockDateTime().second;
+
+  inline function get_timeOfDay() : Time
+    return clockDateTime().timeOfDay;
 }
