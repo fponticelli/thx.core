2b50b3bb31810ed886c855db167142f0820ed00c
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-31 21:10:26 -0600

    added Macros.getModulePath and Macros.getModuleDirectory

--- src/thx/macro/Macros.hx
+++ src/thx/macro/Macros.hx
@@ -58,5 +58,43 @@ with the substituion of symbol with witnExpr expression.
     }
   }
 
+/**
+It returns all the directories that contain (or might contain) modules for the
+specified `pack`. The search is made by default in `Context.getClassPath`, but
+can be restricted to a certain list of `paths`.
+*/
+  public static function getPackageDirectories(pack : String, ?paths : Array<String>) : Array<String> {
+    var cps = null == paths ? Context.getClassPath() : paths,
+        path : Path = pack.split(".").join("/"),
+        results : Array<String> = [];
+    for(cp in cps) {
+      var fp = ((cp : Path) / path).toString();
+      if(sys.FileSystem.exists(fp) && sys.FileSystem.isDirectory(fp))
+        results.push(fp);
+    }
+    return results;
+  }
+
+/**
+Given a `pack`age path, it returns all the modules contained in there.
+The returned values have a fully qualified identifier for the module (it
+includes the package and the module name).
+
+The search is made by default in `Context.getClassPath`, but
+can be restricted to a certain list of `paths`.
+*/
+  public static function getPackageModules(pack : String, ?paths : Array<String>) : Array<String> {
+    var dirs = getPackageDirectories(pack, paths),
+        results = [];
+    for(dir in dirs) {
+      var r = sys.FileSystem.readDirectory(dir);
+      for(file in r) {
+        if(file.endsWith(".hx")) {
+          results.push(pack + "." + file.substring(0, file.length-3));
+        }
+      }
+    }
+    return results;
+  }
 }
 #end
