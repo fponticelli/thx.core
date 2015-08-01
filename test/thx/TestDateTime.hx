355348122d8a2fed39413adf2e8522d933ca4b81
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-08-01 15:11:35 -0600

    more tests for DateTime

--- test/thx/TestDateTime.hx
+++ test/thx/TestDateTime.hx
@@ -4,7 +4,26 @@ import utest.Assert;
 import thx.DateTime;
 
 class TestDateTime {
-  public function new() {}
+  var offset : Time;
+  var date : DateTime;
+
+  public function new() {
+    offset = Time.fromHours(-6);
+    date = DateTime.create(2015, 7, 26, 21, 40, 30, 0, offset);
+  }
+
+  public function testFromToString() {
+    var d : String = date;
+    Assert.isTrue(date == d);
+  }
+
+  public function testFromString() {
+    var offset = Time.fromHours(6);
+    var date = DateTime.create(2015, 7, 26, 21, 40, 30, 0, offset);
+
+    var d : String = date;
+    Assert.isTrue(date == d);
+  }
 
   public function testLocalOffset() {
     var ref   = DateHelper.localOffset(),
