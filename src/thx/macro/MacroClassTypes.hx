22df6f8ee45903191ed248d38c4ef0166c9a3317
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-08-05 16:37:43 -0600

    added MacroClassTypes meta methods

--- src/thx/macro/MacroClassTypes.hx
+++ src/thx/macro/MacroClassTypes.hx
@@ -26,5 +26,38 @@ Returns true if the `cls` is public.
   public static function isPublic(cls : ClassType)
     return !cls.isPrivate;
 
+/**
+Returns true if the `cls` has a meta with the supplied `name`.
+**/
+  public static function hasMeta(cls : ClassType, name : String) {
+    var meta = cls.meta.get();
+    if(null == meta) return false;
+    return Arrays.any(meta, function(entry) return entry.name == name);
+  }
+
+/**
+Extracts the first meta entry parameter for `name`. It returns `null` if it
+does not exist.
+*/
+  public static function getFirstMetaParam(cls : ClassType, name : String)
+    return getMetaParams(cls, name)[0];
+
+/**
+Returns the params of a meta with `name`. It always returns an array even if
+no matches are found.
+*/
+  public static function getMetaParams(cls : ClassType, name : String) {
+    var entry = getMetaEntry(cls, name);
+    if(null == entry) return [];
+    return entry.params;
+  }
+/**
+Returns a `MetaEntry` with `name` if it exists or `null` otherwise.
+*/
+  public static function getMetaEntry(cls : ClassType, name : String) {
+    var meta = cls.meta.get();
+    if(null == meta) return null;
+    return Arrays.find(meta, function(entry) return entry.name == name);
+  }
 }
 #end
