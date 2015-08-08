fd8dd1fd11ae1fe3b4fe56ea4de71500432fed23
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-08-08 11:38:58 -0600

    more tests

--- test/thx/TestTime.hx
+++ test/thx/TestTime.hx
@@ -27,14 +27,15 @@ class TestTime {
 
   public function testFromString() {
     var time : Time = "125:55:45.123";
-    Assert.equals(  5, time.days);
-    Assert.equals(  5, time.hours);
-    Assert.equals( 55, time.minutes);
-    Assert.equals( 45, time.seconds);
-    Assert.equals(123, time.milliseconds);
+    Assert.equals(   5,  time.days);
+    Assert.equals('125', time.totalHours.toStr());
+    Assert.equals(   5,  time.hours);
+    Assert.equals(  55,  time.minutes);
+    Assert.equals(  45,  time.seconds);
+    Assert.equals( 123,  time.milliseconds);
 
     Assert.equals("125:55:45.123", time.toString());
-    Assert.equals("3.17:25:30.005", ("3.17:25:30.005" : Time).toString());
-    Assert.equals("-3.17:25:30.05", ("-3.17:25:30.05" : Time).toString());
+    Assert.equals("89:25:30.005", ("3.17:25:30.005" : Time).toString());
+    Assert.equals("-89:25:30.05", ("-3.17:25:30.05" : Time).toString());
   }
 }
