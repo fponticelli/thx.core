67e36f84d4ccca5fff384ed04790d3106335d21d
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-08-08 11:01:33 -0600

    parse negative times and times with day prefixes

--- test/thx/TestTime.hx
+++ test/thx/TestTime.hx
@@ -34,5 +34,7 @@ class TestTime {
     Assert.equals(123, time.milliseconds);
 
     Assert.equals("125:55:45.123", time.toString());
+    Assert.equals("3.17:25:30.005", ("3.17:25:30.005" : Time).toString());
+    Assert.equals("-3.17:25:30.05", ("-3.17:25:30.05" : Time).toString());
   }
 }
