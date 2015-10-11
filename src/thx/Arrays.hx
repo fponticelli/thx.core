d00207587d030da01e91f62b6efa9d6a829dc577
Trent McPheron <twilightinzero@gmail.com>
2015-10-11 18:59:11 -0400

    Create Arrays.containsAll. Returns true if all elements are in the array.

--- src/thx/Arrays.hx
+++ src/thx/Arrays.hx
@@ -130,6 +130,18 @@ An optional equality function can be passed as the last argument. If not provide
   }
 
 /**
+Returns `true` if all elements in `elements` are found in the array.
+
+An optional equality function can be passed as the last argument. If not provided, strict equality is adopted.
+**/
+ public static function containsAll<T>(array : Array<T>, elements : Iterable<T>, ?eq : T -> T -> Bool) : Bool {
+  for (el in elements) {
+    if (!contains(array, el, eq)) return false;
+  }
+  return true;
+ }
+
+/**
 Returns `true` if any element in `elements` is found in the array.
 
 An optional equality function can be passed as the last argument. If not provided, strict equality is adopted.
