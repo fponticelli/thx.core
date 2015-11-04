f9d82605fead7f6b2d8c78b62c179015bfaa54a4
Kris Nuttycombe <kris.nuttycombe@pellucid.com>
2015-11-04 10:44:45 -0700

    Add function to find both extrema of an iterable.
    This also adds an Ord<DateTime> instance as well
    as TreeBag#flatten.

--- src/thx/Ord.hx
+++ src/thx/Ord.hx
@@ -12,4 +12,12 @@ abstract Ord<A> (A -> A -> Ordering) from A -> A -> Ordering to A -> A -> Orderi
 
   public function contramap<B>(f: B -> A): Ord<B> 
     return function(b0, b1) { return this(f(b0), f(b1)); };
+
+  public function inverse(): Ord<A> return function(a0: A, a1: A) { return this(a1, a0); };
+
+  public static function fromCompare<A>(f: A -> A -> Int): Ord<A> 
+    return function(a0: A, a1: A) {
+      var i = f(a0, a1);
+      return if (i < 0) LT else if (i == 0) EQ else GT;
+    };
 }
