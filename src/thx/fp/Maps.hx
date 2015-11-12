bcd4203daf12cc7038b95a03ad76bcdd1ba27eba
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-11-12 14:36:34 -0700

    added convenience methods for known types for map conversion, closes issue #108

--- src/thx/fp/Maps.hx
+++ src/thx/fp/Maps.hx
@@ -3,47 +3,64 @@ package thx.fp;
 import haxe.ds.Option;
 using thx.Ord;
 using thx.Options;
+#if (haxe_ver >= 3.200)
+import haxe.Constraints.IMap;
+#else
+import Map.IMap;
+#end
 
 class StringMap {
+  static var comparator = Strings.compare.fromIntComparison();
   inline static public function exists<V>(map : Map<String, V>, key : String) : Bool
-    return map.lookup(key, Strings.compare.fromIntComparison()).toBool();
+    return map.lookup(key, comparator).toBool();
 
   inline static public function get<V>(map : Map<String, V>, key : String) : Option<V>
-    return map.lookup(key, Strings.compare.fromIntComparison());
+    return map.lookup(key, comparator);
 
   inline static public function getAlt<V>(map : Map<String, V>, key : String, alt : V) : V
-    return map.lookup(key, Strings.compare.fromIntComparison()).getOrElse(alt);
+    return map.lookup(key, comparator).getOrElse(alt);
 
   inline static public function set<V>(map : Map<String, V>, key : String, value : V) : Map<String, V>
-    return map.insert(key, value, Strings.compare.fromIntComparison());
+    return map.insert(key, value, comparator);
+
+  public static function fromNative<V>(map : IMap<String, V>) : Map<String, V>
+    return Map.fromNative(map, comparator);
 }
 
 class FloatMap {
+  static var comparator = Floats.compare.fromIntComparison();
   inline static public function exists<V>(map : Map<Float, V>, key : Float) : Bool
-    return map.lookup(key, Floats.compare.fromIntComparison()).toBool();
+    return map.lookup(key, comparator).toBool();
 
   inline static public function get<V>(map : Map<Float, V>, key : Float) : Option<V>
-    return map.lookup(key, Floats.compare.fromIntComparison());
+    return map.lookup(key, comparator);
 
   inline static public function getAlt<V>(map : Map<Float, V>, key : Float, alt : V) : V
-    return map.lookup(key, Floats.compare.fromIntComparison()).getOrElse(alt);
+    return map.lookup(key, comparator).getOrElse(alt);
 
   inline static public function set<V>(map : Map<Float, V>, key : Float, value : V) : Map<Float, V>
     return map.insert(key, value, Floats.compare.fromIntComparison());
+
+  public static function fromNative<V>(map : IMap<Float, V>) : Map<Float, V>
+    return Map.fromNative(map, comparator);
 }
 
 class IntMap {
+  static var comparator = Ints.compare.fromIntComparison();
   inline static public function exists<V>(map : Map<Int, V>, key : Int) : Bool
-    return map.lookup(key, Ints.compare.fromIntComparison()).toBool();
+    return map.lookup(key, comparator).toBool();
 
   inline static public function get<V>(map : Map<Int, V>, key : Int) : Option<V>
-    return map.lookup(key, Ints.compare.fromIntComparison());
+    return map.lookup(key, comparator);
 
   inline static public function getAlt<V>(map : Map<Int, V>, key : Int, alt : V) : V
-    return map.lookup(key, Ints.compare.fromIntComparison()).getOrElse(alt);
+    return map.lookup(key, comparator).getOrElse(alt);
 
   inline static public function set<V>(map : Map<Int, V>, key : Int, value : V) : Map<Int, V>
     return map.insert(key, value, Ints.compare.fromIntComparison());
+
+  public static function fromNative<V>(map : IMap<Int, V>) : Map<Int, V>
+    return Map.fromNative(map, comparator);
 }
 
 class ComparableOrdMap {
@@ -58,6 +75,9 @@ class ComparableOrdMap {
 
   inline static public function set<V, T : ComparableOrd<T>>(map : Map<T, V>, key : T, value : V) : Map<T, V>
     return map.insert(key, value, function(a : T, b : T) return a.compareTo(b));
+
+  public static function fromNative<V, T : ComparableOrd<T>>(map : IMap<T, V>) : Map<T, V>
+    return Map.fromNative(map, function(a : T, b : T) return a.compareTo(b));
 }
 
 class ComparableMap {
@@ -72,4 +92,7 @@ class ComparableMap {
 
   inline static public function set<V, T : Comparable<T>>(map : Map<T, V>, key : T, value : V) : Map<T, V>
     return map.insert(key, value, function(a : T, b : T) : Ordering return a.compareTo(b).fromInt());
+
+  public static function fromNative<V, T : Comparable<T>>(map : IMap<T, V>) : Map<T, V>
+    return Map.fromNative(map, function(a : T, b : T) return a.compareTo(b).fromInt());
 }
