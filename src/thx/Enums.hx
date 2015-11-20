f7d89b9bda53bd1b130e363e3c206855fbd3fa8c
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-11-19 21:49:22 -0700

    better typing for Enums

--- src/thx/Enums.hx
+++ src/thx/Enums.hx
@@ -7,7 +7,7 @@ class Enums {
 /**
 Converts an enum value into a `String` representation.
 **/
-  public static function string(e : Dynamic) {
+  public static function string<T : EnumValue>(e : T) {
     var cons = Type.enumConstructor(e);
     var params = [];
     for (param in Type.enumParameters(e))
@@ -20,10 +20,32 @@ Compares two enum values. Comparison is based on the constructor definition
 index. If `a` and `b` are the same constructor and have parameters, parameters
 are compared using the same rules applied for `thx.Arrays.compare`.
 **/
-  public static function compare(a : Dynamic, b : Dynamic) {
+  public static function compare<T : EnumValue>(a : T, b : T) : Int {
     var v = Ints.compare(Type.enumIndex(a), Type.enumIndex(b));
     if (v != 0)
       return v;
     return Arrays.compare(Type.enumParameters(a), Type.enumParameters(b));
+  }
+
+/**
+Returns the lower between two enum instances. Sequence is determined by their
+index in the type definition.
+**/
+  public static function min<T : EnumValue>(a : T, b : T) : T {
+    if(compare(a, b) < 0)
+      return a;
+    else
+      return b;
+  }
+
+/**
+Returns the higher between two enum instances. Sequence is determined by their
+index in the type definition.
+**/
+  public static function max<T : EnumValue>(a : T, b : T) : T {
+    if(compare(a, b) > 0)
+      return a;
+    else
+      return b;
   }
 }
