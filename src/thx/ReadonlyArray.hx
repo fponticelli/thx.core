02c8924ddc9dc63b030ea6a2db4c4e898935b813
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-11-24 14:21:09 -0700

    added ReadonlyArrays.empty

--- src/thx/ReadonlyArray.hx
+++ src/thx/ReadonlyArray.hx
@@ -3,6 +3,9 @@ package thx;
 @:forward(length, concat, copy, filter, indexOf, iterator, join, lastIndexOf,
           map, slice, toString)
 abstract ReadonlyArray<T>(Array<T>) from Array<T> {
+  inline public static function empty<T>() : ReadonlyArray<T>
+    return [];
+
   @:arrayAccess
   inline function get(index : Int)
     return this[index];
