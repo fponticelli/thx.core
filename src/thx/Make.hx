50925f3cc2ec87dda7400ed34c2154eaaa3b2bad
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-12-02 17:36:38 -0700

    Make.constructor can now refer a typedef

--- src/thx/Make.hx
+++ src/thx/Make.hx
@@ -1,25 +1,39 @@
 package thx;
 
 import haxe.macro.Expr;
+import haxe.macro.ExprTools;
+import haxe.macro.Type;
+import haxe.macro.TypeTools;
 import haxe.macro.Context;
 
 class Make {
   macro public static function constructor(expr : ExprOf<{}>) {
-    switch expr.expr {
+    var items = switch expr.expr {
       case EObjectDecl(fields):
-        var items  = fields.map(extractField),
-            args   = items.map(function(item) return '${item.field} : ${item.type}'),
-            assign = items.map(function(item) return '${item.field} : ${item.field}'),
-            fun    = 'function constructor(${args.join(", ")}) return {\n  ${assign.join(",\n")}\n}';
-        return Context.parse(fun, Context.currentPos());
-      case _:
-        Context.error("Make.constructor only accepts anonymous objects with type names as values", Context.currentPos());
+        fields.map(extractFieldFromLiteral);
+      case EConst(CIdent(type)):
+        switch Context.getType(type) {
+          case TType(t, p):
+            switch t.get().type {
+              case TAnonymous(anonym):
+                anonym.get().fields.map(extractFieldAnonymous);
+              case _:
+                Context.error("Make.constructor can only take a reference to a typedef that represent an object literal", Context.currentPos()); [];
+            }
+          case _:
+            Context.error("Make.constructor can only take a reference to a typedef that represent an object literal", Context.currentPos()); [];
+        }
+      case other:
+        Context.error("Make.constructor only accepts anonymous objects with type names as values or a reference to a typedef", Context.currentPos()); [];
     }
-    return macro null;
+    var args   = items.map(function(item) return '${item.field} : ${item.type}'),
+        assign = items.map(function(item) return '${item.field} : ${item.field}'),
+        fun    = 'function constructor(${args.join(", ")}) return {\n  ${assign.join(",\n")}\n}';
+    return Context.parse(fun, Context.currentPos());
   }
 
 #if macro
-  static function extractField(field) {
+  static function extractFieldFromLiteral(field) {
     switch field.expr.expr {
       case EConst(CIdent(type)):
         return { field : field.field, type : type };
@@ -27,6 +41,10 @@ class Make {
         Context.error("Make.constructor fields can only have values that represent types", Context.currentPos());
         return null;
     }
+  }
+
+  static function extractFieldAnonymous(field : ClassField) {
+    return { field : field.name, type : TypeTools.toString(field.type) };
   }
 #end
 }
--- test/thx/TestMake.hx
+++ test/thx/TestMake.hx
@@ -5,7 +5,7 @@ import utest.Assert;
 class TestMake {
   public function new() {}
 
-  public function testConstructor() {
+  public function testConstructorLiteral() {
     var f = Make.constructor({ a : String, b : Int, c : Float, d : String });
     Assert.same({
       a : "A",
@@ -14,4 +14,21 @@ class TestMake {
       d : "D"
     }, f("A", 1, 0.2, "D"));
   }
+
+  public function testConstructorFromTypedef() {
+    var f = Make.constructor(ConstructorType);
+    Assert.same({
+      a : "A",
+      b : 1,
+      c : 0.2,
+      d : "D"
+    }, f("A", 1, 0.2, "D"));
+  }
+}
+
+typedef ConstructorType = {
+  a : String,
+  b : Int,
+  c : Float,
+  d : String
 }
