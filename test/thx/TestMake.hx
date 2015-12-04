1f2cd4b12650a27733e3b389d85da776bc9afef2
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-12-04 16:57:38 -0700

    make possible to set field sequence in Make.constructor

--- test/thx/TestMake.hx
+++ test/thx/TestMake.hx
@@ -6,11 +6,11 @@ class TestMake {
   public function new() {}
 
   public function testConstructorLiteral() {
-    var f = Make.constructor({ a : String, b : Int, c : Float, d : String });
+    var f = Make.constructor({ c : String, b : Int, a : Float, d : String });
     Assert.same({
-      a : "A",
+      c : "A",
       b : 1,
-      c : 0.2,
+      a : 0.2,
       d : "D"
     }, f("A", 1, 0.2, "D"));
   }
@@ -18,17 +18,26 @@ class TestMake {
   public function testConstructorFromTypedef() {
     var f = Make.constructor(ConstructorType);
     Assert.same({
-      a : "A",
+      c : "A",
       b : 1,
-      c : 0.2,
+      a : 0.2,
       d : "D"
     }, f("A", 1, 0.2, "D"));
   }
 }
 
+@:sequence(c, b, a, d)
+// typedef ConstructorType = {
+//   var c : String;
+//   var b : Int;
+//   @:optional var a : Float;
+//   var d : String;
+// }
+
+
 typedef ConstructorType = {
-  a : String,
+  c : String,
   b : Int,
-  c : Float,
+  ?a : Float,
   d : String
 }
