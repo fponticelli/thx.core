3e199cc72557b6fa4cae833a3d389f7035c7850f
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-12-07 16:58:55 -0700

    added Convert.toArray and reused to avoid code duplication

--- src/thx/Convert.hx
+++ src/thx/Convert.hx
@@ -127,87 +127,66 @@ class Convert {
     return try toObject(value) catch(e : Error) alt;
   }
 
-  public static function toArrayString(value : Dynamic) : Array<String> {
-    if(null == value) return null;
-    return Std.is(value, Array) ?
-      (value : Array<Dynamic>).map(toString) :
-      throw new Error('unable to convert $value to Array<String>');
-  }
+  public static function toArrayString(value : Dynamic) : Array<String>
+    return toArray(value, Convert.toString);
 
   public static function toArrayStringOr(value : Dynamic, alt : Array<String>) : Array<String> {
     if(null == value) return alt;
     return try toArrayString(value) catch(e : Error) alt;
   }
 
-  public static function toArrayInt(value : Dynamic) : Array<Int> {
-    if(null == value) return null;
-    return Std.is(value, Array) ?
-      (value : Array<Dynamic>).map(toInt) :
-      throw new Error('unable to convert $value to Array<Int>');
-  }
+  public static function toArrayInt(value : Dynamic) : Array<Int>
+    return toArray(value, toInt);
 
   public static function toArrayIntOr(value : Dynamic, alt : Array<Int>) : Array<Int> {
     if(null == value) return alt;
     return try toArrayInt(value) catch(e : Error) alt;
   }
 
-  public static function toArrayFloat(value : Dynamic) : Array<Float> {
-    if(null == value) return null;
-    return Std.is(value, Array) ?
-      (value : Array<Dynamic>).map(toFloat) :
-      throw new Error('unable to convert $value to Array<Float>');
-  }
+  public static function toArrayFloat(value : Dynamic) : Array<Float>
+    return toArray(value, toFloat);
 
   public static function toArrayFloatOr(value : Dynamic, alt : Array<Float>) : Array<Float> {
     if(null == value) return alt;
     return try toArrayFloat(value) catch(e : Error) alt;
   }
 
-  public static function toArrayBool(value : Dynamic) : Array<Bool> {
-    if(null == value) return null;
-    return Std.is(value, Array) ?
-      (value : Array<Dynamic>).map(toBool):
-      throw new Error('unable to convert $value to Array<Bool>');
-  }
+  public static function toArrayBool(value : Dynamic) : Array<Bool>
+    return toArray(value, toBool);
 
   public static function toArrayBoolOr(value : Dynamic, alt : Array<Bool>) : Array<Bool> {
     if(null == value) return alt;
     return try toArrayBool(value) catch(e : Error) alt;
   }
 
-  public static function toArrayDate(value : Dynamic) : Array<Date> {
-    if(null == value) return null;
-    return Std.is(value, Array) ?
-      (value : Array<Dynamic>).map(toDate):
-      throw new Error('unable to convert $value to Array<Date>');
-  }
+  public static function toArrayDate(value : Dynamic) : Array<Date>
+    return toArray(value, toDate);
 
   public static function toArrayDateOr(value : Dynamic, alt : Array<Date>) : Array<Date> {
     if(null == value) return alt;
     return try toArrayDate(value) catch(e : Error) alt;
   }
 
-  public static function toArrayDateTime(value : Dynamic) : Array<DateTime> {
-    if(null == value) return null;
-    return Std.is(value, Array) ?
-      (value : Array<Dynamic>).map(toDateTime):
-      throw new Error('unable to convert $value to Array<DateTime>');
-  }
+  public static function toArrayDateTime(value : Dynamic) : Array<DateTime>
+    return toArray(value, toDateTime);
 
   public static function toArrayDateTimeOr(value : Dynamic, alt : Array<DateTime>) : Array<DateTime> {
     if(null == value) return alt;
     return try toArrayDateTime(value) catch(e : Error) alt;
   }
 
-  public static function toArrayObject(value : Dynamic) : Array<{}> {
-    if(null == value) return null;
-    return Std.is(value, Array) ?
-      (value : Array<Dynamic>).map(toObject):
-      throw new Error('unable to convert $value to Array<{}>');
-  }
+  public static function toArrayObject(value : Dynamic) : Array<{}>
+    return toArray(value, toObject);
 
   public static function toArrayObjectOr(value : Dynamic, alt : Array<{}>) : Array<{}> {
     if(null == value) return alt;
     return try toArrayObject(value) catch(e : Error) alt;
+  }
+
+  public static function toArray<T>(value : Dynamic, convert : Dynamic -> T) : Array<T> {
+    if(null == value) return null;
+    return Std.is(value, Array) ?
+      (value : Array<Dynamic>).map(convert):
+      throw new Error('unable to convert $value to Array<T>');
   }
 }
