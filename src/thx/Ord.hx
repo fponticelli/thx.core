010690c46629d42b63698bdff6fdb3283b6cc346
Kris Nuttycombe <kris.nuttycombe@pellucid.com>
2016-02-04 10:51:19 -0700

    Add min, max, and equal to Ord.

--- src/thx/Ord.hx
+++ src/thx/Ord.hx
@@ -28,24 +28,33 @@ abstract Ord<A> (A -> A -> Ordering) from A -> A -> Ordering to A -> A -> Orderi
   inline public function order(a0: A, a1: A): Ordering
     return this(a0, a1);
 
+  inline public function max(a0: A, a1: A): A
+    return switch this(a0, a1) {
+      case LT | EQ: a1;
+      case GT: a0;
+    };
+
+  inline public function min(a0: A, a1: A): A
+    return switch this(a0, a1) {
+      case LT | EQ: a0;
+      case GT: a1;
+    };
+
+  inline public function equal(a0: A, a1: A): Bool
+    return this(a0, a1) == EQ;
+
   public function contramap<B>(f: B -> A): Ord<B>
     return function(b0, b1) { return this(f(b0), f(b1)); };
 
-  public function inverse(): Ord<A> return function(a0: A, a1: A) { return this(a1, a0); };
-
+  public function inverse(): Ord<A> 
+    return function(a0: A, a1: A) { return this(a1, a0); };
 
   public static function fromIntComparison<A>(f : A -> A -> Int) : Ord<A>
-    return function(a : A, b : A) {
-      return Ordering.fromInt(f(a, b));
-    };
+    return function(a : A, b : A) { return Ordering.fromInt(f(a, b)); };
 
   public static function forComparable<T : Comparable<T>>(): Ord<T>
-    return function(a: T, b: T) {
-      return Ordering.fromInt(a.compareTo(b));
-    };
+    return function(a: T, b: T) { return Ordering.fromInt(a.compareTo(b)); };
 
   public static function forComparableOrd<T : ComparableOrd<T>>(): Ord<T>
-    return function(a: T, b: T) {
-      return a.compareTo(b);
-    };
+    return function(a: T, b: T) { return a.compareTo(b); };
 }
