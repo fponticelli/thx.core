0fc96e6776d5819024c7fd7cafa74f9e94cecc41
Andy White <greatwhitehope@gmail.com>
2016-04-12 15:54:33 -0600

    Add BitMatrix operators

--- test/thx/TestBitMatrix.hx
+++ test/thx/TestBitMatrix.hx
@@ -17,31 +17,29 @@ class TestBitMatrix {
 
   public function testBitMatrix() {
     var bits = new BitMatrix(3, 3);
-    bits[0][0] = true;
-    bits[0][1] = true;
-    bits[0][2] = true;
-    bits[1][0] = false;
-    bits[1][1] = false;
-    bits[1][2] = false;
-    bits[2][0] = true;
-    bits[2][1] = false;
-    bits[2][2] = true;
-    Assert.equals('111', bits[0].toString());
-    Assert.equals('000', bits[1].toString());
-    Assert.equals('101', bits[2].toString());
+    bits.setBitAt(0, 0, true);
+    bits.setBitAt(0, 1, true);
+    bits.setBitAt(0, 2, true);
+    bits.setBitAt(1, 0, false);
+    bits.setBitAt(1, 1, false);
+    bits.setBitAt(1, 2, false);
+    bits.setBitAt(2, 0, true);
+    bits.setBitAt(2, 1, false);
+    bits.setBitAt(2, 2, true);
+    Assert.equals('111,000,101', bits.toString());
   }
 
   public function testClone() {
     var bits = new BitMatrix(2, 3);
-    bits[0][0] = true;
-    bits[0][1] = true;
-    bits[0][2] = true;
-    bits[1][0] = false;
-    bits[1][1] = false;
-    bits[1][2] = false;
+    bits.setBitAt(0, 0, true);
+    bits.setBitAt(0, 1, true);
+    bits.setBitAt(0, 2, true);
+    bits.setBitAt(1, 0, false);
+    bits.setBitAt(1, 1, false);
+    bits.setBitAt(1, 2, false);
     var clone = bits.clone();
-    clone[0][1] = false;
-    clone[1][1] = true;
+    clone.setBitAt(0, 1, false);
+    clone.setBitAt(1, 1, true);
     Assert.same('111,000', bits.toString());
     Assert.same('101,010', clone.toString());
   }
@@ -54,5 +52,36 @@ class TestBitMatrix {
   public function testFromToBools() {
     var bits = BitMatrix.fromBools([[true, true], [false, false], [true, false], [false, true]]);
     Assert.same('11,00,10,01', bits.toString());
+  }
+
+  public function testAnd() {
+    var b1 = BitMatrix.fromString('11,00');
+    var b2 = BitMatrix.fromString('10,10');
+    var actual = b1 & b2;
+    var expected = BitMatrix.fromString('10,00');
+    Assert.isTrue(expected.equals(actual));
+  }
+
+  public function testOr() {
+    var b1 = BitMatrix.fromString('11,00');
+    var b2 = BitMatrix.fromString('10,10');
+    var actual = b1 | b2;
+    var expected = BitMatrix.fromString('11,10');
+    Assert.isTrue(expected.equals(actual));
+  }
+
+  public function testXor() {
+    var b1 = BitMatrix.fromString('11,00');
+    var b2 = BitMatrix.fromString('10,10');
+    var actual = b1 ^ b2;
+    var expected = BitMatrix.fromString('01,10');
+    Assert.isTrue(expected.equals(actual));
+  }
+
+  public function testNegate() {
+    var b1 = BitMatrix.fromString('11,00');
+    var actual = ~b1;
+    var expected = BitMatrix.fromString('00,11');
+    Assert.isTrue(expected.equals(actual));
   }
 }
