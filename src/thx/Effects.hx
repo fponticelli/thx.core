d825b9e6721b89e0cf1d460088607775c408c8a2
Kris Nuttycombe <kris.nuttycombe@pellucid.com>
2016-04-14 15:22:44 -0600

    Utility functions for ordering and tracing.

--- src/thx/Effects.hx
+++ src/thx/Effects.hx
@@ -10,6 +10,11 @@ class Effects {
     return input;
   }
 
+  inline public static function traced<T, TAny>(input: T, f: T -> String): T {
+    trace(f(input));
+    return input;
+  }
+
   inline public static function log<T>(input : T, ?pos : PosInfos) : T
     return tap(input, logger.bind(_, pos));
 }
