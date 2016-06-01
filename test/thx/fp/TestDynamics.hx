2f6f3efa548becbfc6a7256e5ffb3c31a093fdaa
Kris Nuttycombe <kris.nuttycombe@pellucid.com>
2016-06-01 10:00:53 -0600

    Make parseStringMap parser argument order agree with parseArrayIndexed.

--- test/thx/fp/TestDynamics.hx
+++ test/thx/fp/TestDynamics.hx
@@ -15,6 +15,6 @@ class TestDynamics {
     var sample = { t: 1, u: 2, v: 3 };
     var expected: StringMap<Int> = [ "t" => 1, "u" => 2, "v" => 3 ];
 
-    Assert.same(Right(expected), parseStringMap(sample, function(s, v) return parseInt(v), identity));
+    Assert.same(Right(expected), parseStringMap(sample, function(v, _) return parseInt(v), identity));
   }
 }
