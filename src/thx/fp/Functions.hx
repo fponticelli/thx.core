672547e7eebbb892d3de028af61a3a256e6f553f
Kris Nuttycombe <kris.nuttycombe@pellucid.com>
2016-07-28 11:26:00 -0600

    Add 2- and 3-arity flip functions to thx.fp.Functions

--- src/thx/fp/Functions.hx
+++ src/thx/fp/Functions.hx
@@ -12,4 +12,14 @@ class Functions {
     return function(b: B) : A -> C {
       return function(a: A) : C return f(a)(b);
     };
+
+  inline public static function flip2<A, B, C>(f: A -> B -> C): B -> A -> C
+    return function(b: B, a: A): C {
+      return f(a, b);
+    };
+
+  inline public static function flip3<A, B, C, D>(f: A -> B -> C -> D): B -> A -> C -> D
+    return function(b: B, a: A, c: C): D {
+      return f(a, b, c);
+    };
 }
