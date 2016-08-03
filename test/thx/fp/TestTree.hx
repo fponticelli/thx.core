199bb256fb0f39dfff0fc1a4e34a91660d7a74c4
0b1kn00b <polysemantic@gmail.com>
2016-08-03 10:27:07 +0200

    major functions of Tree and TreeZip covered

--- test/thx/fp/TestTree.hx
+++ test/thx/fp/TestTree.hx
@@ -1,7 +1,9 @@
 package thx.fp;
 
 import utest.Assert;
+import thx.fp.List;
 import thx.fp.Tree;
+import thx.fp.tree.TreeZip;
 
 @:access(thx.fp) class TestTree {
   public function new() {}
@@ -11,6 +13,45 @@ import thx.fp.Tree;
       [Branch('l',[Branch('ll'),Branch("lr")]),Branch('m',[Branch('ml'),Branch('mm'),Branch('mr')]),Branch('r')]
     );
   }
+  public function testZipNavigation(){
+    var t : TreeZip<String> = construct();
+    Assert.isTrue(t.isRoot());
+    Assert.equals('root',t.value());
+    var l = t.down();
+    Assert.equals('l',l.value());
+    var m = l.right();
+    Assert.equals('m',m.value());
+    var root = m.up();
+    Assert.equals('root',root.value());
+    var l = m.left();
+    Assert.equals('l',l.value());
+  }
+  public function testZipEdit(){
+    var t : TreeZip<String> = construct();
+    var leaf = t.down().down();
+    var folded = leaf.foldLeft(
+      List.empty(),
+      function(memo,next){
+        //trace(next);
+        return Cons(next,memo);
+      }
+    );
+    var updated = TreeZip.update(leaf,Branch("w00t"));
+    Assert.equals('w00t',updated.up().down().value());
+    Assert.equals('w00t',updated.up().up().down().down().value());
+    Assert.equals('l',updated.up().value());
+    Assert.equals('root',updated.up().up().value());
+  }
+  public function testZipMod(){
+    var t : TreeZip<String> = construct();
+    var l   = t.down();
+    var ll  = l.down();
+    var without_ll = l.remChildNode(ll.head());
+    Assert.equals(null,without_ll.up().down().down().value());
+    var add_ll = without_ll.addChildNode(ll.head());
+    Assert.equals('ll',add_ll.up().down().down().value());
+  }
+
   public function testGen(){
     var tree  = construct();
     var df    = 'rootllllrmmlmmmrr';
@@ -80,7 +121,7 @@ import thx.fp.Tree;
     //trace("________________");
     root = m.up();
     Assert.equals(0,root.path.length);
-  
+
     m = mm.up();
     trace(m);
     Assert.equals('m',m.value());
