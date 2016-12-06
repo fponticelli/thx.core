b6fa639db9fc8d6a12a3570a5518a359a0ae0c9d
Andy White <greatwhitehope@gmail.com>
2016-12-06 16:55:05 -0700

    Change thx.Any to remove implicit conversions

--- src/thx/Any.hx
+++ src/thx/Any.hx
@@ -1,19 +1,23 @@
 package thx;
 
+#if (haxe_ver >= 3.4)
+import Any as HaxeAny;
+#end
+
+
 /**
   Safer version of Haxe Dynamic that prevents common issues with Dynamics by forcing
   you to explicitly convert it to the desired type before doing any operations on it.
 
-  Taken from Haxe tink_core library by Juraj Kirchheim
-  - https://github.com/haxetink/tink_core#any
-  - https://github.com/haxetink/tink_core/blob/master/src/tink/core/Any.hx
+  Note: this Any type differs from the Haxe Any type (in 3.4 or later) in how implicit
+  conversions to and from Dynamic are handled.
 **/
-#if haxe < 3.4
 abstract Any(Dynamic) {
-  @:noCompletion @:extern @:to inline function __promote<T>():T return this;
-  @:noCompletion @:extern @:from inline static function __cast<T>(value:T):Any return cast value;
-}
-#else
-import Any as HaxeAny;
-typedef Any = HaxeAny;
+  @:from inline public static function ofValue<T>(value:T):Any return cast value;
+  inline public function unsafeCast<T>():T return this;
+
+#if (haxe_ver >= 3.4)
+  @:from inline public static function fromHaxeAny(haxeAny : HaxeAny) : Any return cast haxeAny;
+  inline public function toHaxeAny() : HaxeAny return cast this;
 #end
+}
