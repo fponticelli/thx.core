eb2a866c1266e3edbb83546b474ac7dedc198cd2
Jens Fischer <jensfischer95@gmail.com>
2019-05-28 22:25:32 +0200

    Fix compatibility with Haxe dev

--- src/thx/Iterators.hx
+++ src/thx/Iterators.hx
@@ -51,9 +51,6 @@ An optional equality function can be passed as the last argument. If not provide
       if(!equality(a.next(), b.next()))
         return false;
     }
-    #if haxe < 3.3
-    return true;
-    #end
   }
 
 /**
